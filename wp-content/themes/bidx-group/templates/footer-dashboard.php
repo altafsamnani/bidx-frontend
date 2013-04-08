da7c18c1ee61bae5e560b575bb4b6c4d9fa85de7
mattijs.spierings <mattijs.spierings@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-04-08 14:12:21 +0000

    updated bidx-group theme

---
+++ wp-content/themes/bidx-group/templates/footer-dashboard.php
@@ -0,0 +1,13 @@
+<footer>
+	<div class="container">
+		DASHBOARD FOOTER
+	</div>
+	<div class="sub-footer">
+			<ul>
+				<li>@ 2013. bidX.net. All rights reserved</li>
+				<li><a href="#">Terms of Service</a></li>
+				<li><a href="#">Privacy</a></li>
+				
+			</ul>
+		</div>
+</footer>
