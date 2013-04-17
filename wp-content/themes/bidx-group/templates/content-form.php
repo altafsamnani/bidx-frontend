b077d1380410b2fe7b407b645fd67847ac197976
altaf.samnani <altaf.samnani@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-04-17 12:50:13 +0000

    Remove handler hardcoded from jquery

--- wp-content/themes/bidx-group/templates/content-form.php
+++ wp-content/themes/bidx-group/templates/content-form.php
@@ -5,36 +5,24 @@
 		
 		<?php while (have_posts()) : the_post(); ?>
 		  <?php
-       //the_content();
-      $bidxUname = urldecode($wp_query->query_vars['buname']);
-      $bidxGid   = urldecode($wp_query->query_vars['bgid']);
-      $bidxGroupName   = urldecode($wp_query->query_vars['bname']);
+       the_content();
+       
+    //  $bidxUname = urldecode($wp_query->query_vars['buname']);
+   //   $bidxGid   = urldecode($wp_query->query_vars['bgid']);
+     // $bidxGroupName   = urldecode($wp_query->query_vars['bname']);
       
-      $content = get_the_content( );
-      $content = apply_filters('the_content', $content);
-      $content = str_replace(']]>', ']]&gt;', $content);
-      $content = str_replace('[!EMAIL!]', $bidxUname, $content);
-      $content = str_replace('[!GROUPNAME!]', $bidxGroupName, $content);
-      echo $content;
+//      $content = get_the_content( );
+//      $content = apply_filters('the_content', $content);
+//      $content = str_replace(']]>', ']]&gt;', $content);
+//      $content = str_replace('[!EMAIL!]', $bidxUname, $content);
+//      $content = str_replace('[!GROUPNAME!]', $bidxGroupName, $content);
+//      echo $content;
 
       ?>
 		  <?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
 		<?php endwhile; ?>
 	</div>
 </div>
-
-<script type="text/javascript">
-	$(function(){
-		$("form").form({
-			callToAction : '.jsCreateGroup',
-			errorClass : 'error',
-			url : '/handler',
-			enablePlugins: ['date','slider']
-		});
-	});
-</script>
-
-
 <?php add_action('wp_footer', 'addToFooter',200);
 
 	function addToFooter() {
