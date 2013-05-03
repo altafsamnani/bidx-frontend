af42bffd4e6b180948b747b7bd4a86d599dd0705
jaap.gorjup <jaap.gorjup@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-05-03 12:32:57 +0000

    Cleanup of rules and generalization. Next fix will be the routing of the bidxaction. Using this mechanism much more url structures can be added dynamically and we just need to keep the naming convention working.

--- wp-content/plugins/bidx-plugin/bidX-plugin.php
+++ wp-content/plugins/bidx-plugin/bidX-plugin.php
@@ -11,34 +11,61 @@ License: Commercial
 
 //Startpage
 //Should redirect from frontpage to /dashboard if user is authenticated
-add_filter('init', 'validateFrontpageLogin');
-function validateFrontpageLogin() {
-	if (is_front_page() && is_user_logged_in()) {
-		wp_redirect('/dashboard/');
+
+add_filter( 'init', 'validateFrontpageLogin' );
+function validate_frontpage_login() {
+	if ( is_front_page() && is_user_logged_in() ) {
+		wp_redirect( '/dashboard/' );
 	}	
 }
 
-//
 //Rewrites for urls
-//
-add_action('init', 'add_bidx_plugin_rewrites');
-function add_bidx_plugin_rewrites() {
-	
-	//Memberprofile app
-	add_rewrite_rule( '^member/([^/]*)/?', plugins_url( 'apps/memberprofile/memberprofile.php?member_id=$matches[1]' ), 'top' );
-	add_rewrite_rule( '^member/?', plugins_url( 'apps/memberprofile/memberprofile.php'),'top');
-	
-	//Businessplan app
-	add_rewrite_rule( '^businessplan/([^/]*)/?', plugins_url( 'apps/businessplan/businessplan.php?bp_id=$matches[1]'), 'top' );
-	add_rewrite_rule( '^businessplan/?', plugins_url( 'apps/businessplan/businessplan.php'), 'top' );
-	
-	//Dashboard app
-	add_rewrite_rule( '^home/([^/]*)/?', plugins_url( 'apps/dashboard/dashboard.php?dashboard_id=$matches[1]'), 'top' );
-	add_rewrite_rule( '^home/?', plugins_url( 'apps/dashboard/dashboard.php' ), 'top' );
-	
-	//Search app
-	add_rewrite_rule( '^search/([^/]*)/?', plugins_url( 'apps/search/search.php?q=$matches[1]'), 'top' );
-	add_rewrite_rule( '^search/?', plugins_url( 'apps/search/search.php'), 'top' );
-	
+//Claims url pattern for the bidx applications
+function add_bidx_rewrite_rule($name, $bidxaction = null, $target = 'top')
+{
+	if ($bidxaction === null) {
+		$bidxaction = $name;
+	}
+	//3rd level optionals?
+	//add_rewrite_rule('^'.$name.'/([^/]*)/[^]?','index.php?bidxaction='.$bidxaction.'&bidx_param1=$matches[1]&bidx_param2=$matches[2]', $target);
+	add_rewrite_rule('^'.$name.'/([^/]*)/?','index.php?bidxaction='.$bidxaction.'&bidx_param1=$matches[1]', $target);
+	add_rewrite_rule('^'.$name.'/?','index.php?bidxaction='.$bidxaction, $target);
 }
+
+function bidxplugin_router() {
+	//retrieve bidx_action param
+	//find file to include and execute
+}
+
+function bidxplugin_activate() {
+	
+	error_log('bidX rules activation started');
+	
+	add_rewrite_tag( '%bidx_action%', '([^&]+)' ); //main action per endpoint
+	add_rewrite_tag( '%bidx_param1%', '([^&]+)' ); //control parameter if available
+	add_rewrite_tag( '%bidx_param2%', '([^&]+)' ); //rest of url data if available
+	
+	add_bidx_rewrite_rule( 'member','memberprofile' );
+	add_bidx_rewrite_rule( 'businessplan' );
+	add_bidx_rewrite_rule( 'mydashboard', 'dashboard' );
+	add_bidx_rewrite_rule( 'search' );
+	
+	//add handler for bidx_action where action is endpoint :
+	//add_action('the_content', bidxplugin_router);
+	
+	error_log('Rewrite rules Bidx activation succeeded');
+
+	flush_rewrite_rules();	
+}
+
+register_activation_hook( __FILE__, 'bidxplugin_activate' );
+
+function bidxplugin_deactivate() {
+
+	error_log('bidX rules de-activation started');	
+	flush_rewrite_rules();
+	//check if they are gone
+	error_log('bidX rules de-activation succeeded');
+}
+register_deactivation_hook( __FILE__, 'bidxplugin_deactivate' );
 ?>
