7901b311a7c75bda1bee48b4b6e4edebf2a294a3
jaap.gorjup <jaap.gorjup@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-05-07 13:33:43 +0000

    Updated plugin php with new structure Added log4php logging

--- wp-content/plugins/bidx-plugin/bidX-plugin.php
+++ wp-content/plugins/bidx-plugin/bidX-plugin.php
@@ -9,7 +9,7 @@ Author URI: http://bidx.net/plugin/bidx-plugin
 License: Commercial
 */
 
-//Generic config for paths
+//Generic config for paths : standard util object?
 if ( !defined( 'BIDX_THEME_DIR' ) ) {
 	define( 'BIDX_THEME_DIR', ABSPATH . 'wp-content/themes/' . get_template() );
 }
@@ -31,84 +31,39 @@ if ( !defined('BIDX_VERSION_NUM') ) {
 }
 add_option( BIDX_VERSION_KEY, BIDX_VERSION_NUM );
 
-//Startpage
-//Should redirect from frontpage to /dashboard if user is authenticated
+require (BIDX_PLUGIN_DIR . '/apps/util.php');
+
+//
+// Activation of frontend apps
+//
+require (BIDX_PLUGIN_DIR . '/apps/rewrite.php');
+require (BIDX_PLUGIN_DIR . '/apps/shortcode.php');
+
+$ruleitems = array( 
+	//				'member' => 'memberprofile',
+	//				'businessplan',
+	//				'mydashboard' => 'dashboard',
+					'search' 
+				  );
+
+$rewrite = new BidxRewrite( $ruleitems );
+register_activation_hook( __FILE__, array( &$rewrite, 'register_rewrite' ) );
+register_deactivation_hook( __FILE__, array( &$rewrite, 'bidxplugin_deactivate' ) );
+
+$shortcode = new BidxShortCode();
+$shortcode -> addMappingArray($ruleitems);
+
+//
+// For the Startpage
+// Should redirect from frontpage to /dashboard if user is authenticated
+// @todo externalize this in redirection handler
+//
 add_filter( 'init', 'validate_frontpage_login' );
 function validate_frontpage_login() {
-	if ( is_front_page() && is_user_logged_in() ) {
-		wp_redirect( '/dashboard/' );
-	}	
+// 	if ( is_front_page() && is_user_logged_in() ) {
+// 		wp_redirect( '/dashboard/' );
+// 	}
+// 	global $wp, $wp_query;
 }
 
-//Rewrites for urls
-//Claims url pattern for the bidx applications
-function add_bidx_rewrite_rule($name, $bidxaction = null, $target = 'top')
-{
-	if ($bidxaction === null) {
-		$bidxaction = $name;
-	}
-	//3rd level optionals?
-	//add_rewrite_rule('^'.$name.'/([^/]*)/[^]?','index.php?post_type=bidx&bidxaction='.$bidxaction.'&bidx_param1=$matches[1]&bidx_param2=$matches[2]', $target);
-	add_rewrite_rule('^'.$name.'/([^/]*)/?','index.php?post_type=bidx&bidxaction='.$bidxaction.'&bidx_param1=$matches[1]', $target);
-	add_rewrite_rule('^'.$name.'/?','index.php?post_type=bidx&bidxaction='.$bidxaction, $target);
-}
-
-function bidxplugin_router() {
-	//retrieve bidx_action param
-	global $wp_query;
-	$bidxaction = $wp_query->query_vars['bidxaction'];
-	add_filter('the_content', array(&$this, 'filter_user_content'), 1);
-	//find file to include and execute
-}
-
-function codex_custom_init() {
-	error_log( 'Initializing custom Post handler' );
-	
-	$args = array(
-			'public' => true,
-			'exclude_from_search' => false,
-			'show_ui' => true, //for managing the plugin in the future?
-			'show_in_menu' => true, //for managing the plugin in the future?
-			'query_var' => true,
-			//			'rewrite' => false, //handled manually for now
-			'rewrite' => array( 'slug' => 'bidx' ),
-			'capability_type' => 'page',
-			'has_archive' => false,
-			'hierarchical' => false,
-			'menu_position' => null,
-			'supports' => array( 'title' )
-	);
-	register_post_type( 'bidx', $args );
-	error_log( 'Custom Post handler ready' );	
-}
-add_action( 'init', 'codex_custom_init' );
-
-function bidxplugin_activate() {
-
-	error_log( 'bidX rules activation started' );
-	
-	add_rewrite_tag( '%bidx_action%', '([^&]+)' ); //main action per endpoint
-	add_rewrite_tag( '%bidx_param1%', '([^&]+)' ); //control parameter if available
-	add_rewrite_tag( '%bidx_param2%', '([^&]+)' ); //rest of url data if available
-	
-	add_bidx_rewrite_rule( 'member','memberprofile' );
-	add_bidx_rewrite_rule( 'businessplan' );
-	add_bidx_rewrite_rule( 'mydashboard', 'dashboard' );
-	add_bidx_rewrite_rule( 'search' );
-		
-	error_log( 'Rewrite rules bidX activation succeeded' );
-	
-	flush_rewrite_rules();	
-}
-
-register_activation_hook( __FILE__, 'bidxplugin_activate' );
-
-function bidxplugin_deactivate() {
-
-	error_log('bidX rules de-activation started');	
-	flush_rewrite_rules();
-	//check if they are gone
-	error_log('bidX rules de-activation succeeded');
-}
-register_deactivation_hook( __FILE__, 'bidxplugin_deactivate' );
 ?>
