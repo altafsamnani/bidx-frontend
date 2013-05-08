8e6fee08395ec12a65867a81a94a015edeac26ac
altaf.samnani <altaf.samnani@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-05-08 15:45:18 +0000

    Indent code

--- wp-content/plugins/bidx-plugin/apps/memberprofile/memberprofile_component.php
+++ wp-content/plugins/bidx-plugin/apps/memberprofile/memberprofile_component.php
@@ -1,20 +1,14 @@
-<?php
-
-
-$view -> render('view-member.phtml');
-
-
-?>
 <div class="block-odd">
 	<div class="container">
 
 		<div class="">
-			<h1>member-profile</h1>
+			<h1>Member-profile</h1>
 		</div>
 
 		<a class="btn btn-primary" href="#edit">Edit</a>
 
-
-<?php $view -> render('edit-member.phtml'); ?>
+<?php
+$view -> render('view-member.phtml');
+$view -> render('edit-member.phtml'); ?>
 	</div>
 </div>
