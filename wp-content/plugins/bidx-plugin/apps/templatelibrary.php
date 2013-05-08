afeb538302ff9743bd9213de843f0e36a4b5e3bf
altaf.samnani <altaf.samnani@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-05-08 14:24:48 +0000

    With comments

--- wp-content/plugins/bidx-plugin/apps/templatelibrary.php
+++ wp-content/plugins/bidx-plugin/apps/templatelibrary.php
@@ -1,13 +1,33 @@
 <?php
+/**
+ * Rendering of variables into template
+ * It renders the variables assigned them through load method through main app.php file
+ *
+ *
+ *
+ * @author Altaf Samnani
+ * @version 1.0
+ */
 class TemplateLibrary {
     protected $template_dir = 'templates/';
     protected $vars = array();
+
+   /**
+	 * Constructor.
+	 * Assign the templates directory
+	 */
     public function __construct($template_dir = null) {
         if ($template_dir !== null) {
             // Check here whether this directory really exists
             $this->template_dir = $template_dir;
         }
     }
+
+   /**
+	 * Renders the file to display content
+	 * @param string $template_file file to display
+	 *
+	 */
     public function render($template_file) {
         if (file_exists($this->template_dir.$template_file)) {
             include $this->template_dir.$template_file;
@@ -15,9 +35,22 @@ class TemplateLibrary {
             throw new Exception('no template file ' . $template_file . ' present in directory ' . $this->template_dir);
         }
     }
+
+   /**
+	 * PHP Magicmethod to assign variables
+	 * @param string $name name of the variable
+   * @param string $value value of the variable
+	 *
+	 */
     public function __set($name, $value) {
         $this->vars[$name] = $value;
     }
+
+    /**
+	 * PHP Magicmethods to get variables value
+	 * @param string $name name of the variable
+	 *
+	 */
     public function __get($name) {
         return $this->vars[$name];
     }
