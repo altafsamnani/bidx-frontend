91904240b82593eaff8e521be94422d712372f9c
altaf.samnani <altaf.samnani@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-05-10 15:52:45 +0000

    Member profile add rows with labels bootstrap

--- wp-content/plugins/bidx-plugin/apps/templatelibrary.php
+++ wp-content/plugins/bidx-plugin/apps/templatelibrary.php
@@ -11,72 +11,89 @@
  */
 class TemplateLibrary {
 
-  protected $template_dir = 'templates/';
-  protected $vars = array();
+    protected $template_dir = 'templates/';
+    protected $vars = array();
 
-  /**
-   * Constructor.
-   * Assign the templates directory
-   */
-  public function __construct($template_dir = null) {
-    if ($template_dir !== null) {
-      // Check here whether this directory really exists
-      $this->template_dir = $template_dir;
+    /**
+     * Constructor.
+     * Assign the templates directory
+     */
+    public function __construct($template_dir = null) {
+        if ($template_dir !== null) {
+            // Check here whether this directory really exists
+            $this->template_dir = $template_dir;
+        }
     }
-  }
 
-  /**
-   * Renders the file to display content
-   * @param string $template_file file to display
-   *
-   */
-  public function render($template_file) {
-    if (file_exists($this->template_dir . $template_file)) {
-      include $this->template_dir . $template_file;
+    /**
+     * Renders the file to display content
+     * @param string $template_file file to display
+     *
+     */
+    public function render($template_file) {
+        if (file_exists($this->template_dir . $template_file)) {
+            include $this->template_dir . $template_file;
+        } else {
+            throw new Exception('no template file ' . $template_file . ' present in directory ' . $this->template_dir);
+        }
     }
-    else {
-      throw new Exception('no template file ' . $template_file . ' present in directory ' . $this->template_dir);
+
+    /**
+     * PHP Magicmethod to assign variables
+     * @param string $name name of the variable
+     * @param string $value value of the variable
+     *
+     */
+    public function __set($name, $value) {
+        $this->vars[$name] = $value;
     }
-  }
 
-  /**
-   * PHP Magicmethod to assign variables
-   * @param string $name name of the variable
-   * @param string $value value of the variable
-   *
-   */
-  public function __set($name, $value) {
-    $this->vars[$name] = $value;
-  }
+    /**
+     * PHP Magicmethods to get variables value
+     * @param string $name name of the variable
+     *
+     */
+    public function __get($name) {
+        return $this->vars[$name];
+    }
 
-  /**
-   * PHP Magicmethods to get variables value
-   * @param string $name name of the variable
-   *
-   */
-  public function __get($name) {
-    return $this->vars[$name];
-  }
+    public function addRows($gridColumnVal, $rowValues, $className = "") {
 
-  public function addRows($gridColumnVal, $rowValues, $className="") {
+        $rowHtml = "<div class='span" . $gridColumnVal . "'>";
+        $className = ($className) ? " class = '" . $className . "' " : '';
 
-    $rowHtml = "<div class='span" . $gridColumnVal . "'>";
-
-    foreach ($rowValues as $label => $values) {
-      if ($values) {
-
-        if(is_string($label)) {
-           $rowHtml = "<div class='span" . $gridColumnVal . "'>";
+        foreach ($rowValues as $values) {
+            if ($values) {
+                $rowHtml .= "<div " . $className . " > " . $values . " </div>";
+            }
         }
 
-        $rowHtml .= "<div class = '".$className." '> ".$values." </div>";
-      }
+        $rowHtml .= "</div>";
+
+        return $rowHtml;
     }
 
-    $rowHtml .= "</div>";
+    public function addRowsWithLabel($gridLabel, $gridValue, $rowValues, $classLabel = "", $classValue = "") {
 
-    return $rowHtml;
-  }
+        $rowHtml = "";
+        $classLabel = ($classLabel) ? " class = '" . $classLabel . "' " : '';
+        $classValue = ($classValue) ? " class = '" . $classValue . "' " : '';
+
+        foreach ($rowValues as $label => $value) {
+            if ($value) {
+                //Display Label
+                $rowHtml .= "<div class='span" . $gridLabel . "'>";
+                $rowHtml .= "<div " . $classLabel . " > " . $label . " </div>";
+                $rowHtml .= "</div>";
+                //Display Value
+                $rowHtml .= "<div class='span" . $gridValue . "'>";
+                $rowHtml .= "<div " . $classValue . " > " . $value . " </div>";
+                $rowHtml .= "</div>";
+            }
+        }
+
+        return $rowHtml;
+    }
 
 }
 
