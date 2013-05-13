db1f958ea432949b8ce79a9504092943b569aa6a
arne.debree <arne.debree@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-05-13 12:44:09 +0000

    disable js script include of plugins.js

--- wp-content/themes/bidx-group/lib/scripts.php
+++ wp-content/themes/bidx-group/lib/scripts.php
@@ -17,7 +17,7 @@
 function roots_scripts() {
   wp_enqueue_style('roots_bootstrap', get_template_directory_uri() . '/assets/css/bootstrap.css', false, null);
   wp_enqueue_style('roots_bootstrap_responsive', get_template_directory_uri() . '/assets/css/bootstrap-responsive.css', array('roots_bootstrap'), null);
-  wp_enqueue_style('main-style', get_template_directory_uri() . '/style.css', false, null);  
+  wp_enqueue_style('main-style', get_template_directory_uri() . '/style.css', false, null);
 wp_enqueue_style('jquery-ui', get_template_directory_uri() . '/assets/css/jqueryUI/ui-darkness/jquery-ui-1.10.2.custom.min.css', false, null);
 
 
@@ -40,11 +40,11 @@ wp_enqueue_style('jquery-ui', get_template_directory_uri() . '/assets/css/jquery
   }
 
   //wp_register_script('modernizr', get_template_directory_uri() . '/assets/js/vendor/modernizr-2.6.2.min.js', false, null, false);
-  wp_register_script('roots_plugins', get_template_directory_uri() . '/assets/js/plugins.js', false, null, true);
+//  wp_register_script('roots_plugins', get_template_directory_uri() . '/assets/js/plugins.js', false, null, true);
   //wp_register_script('roots_main', get_template_directory_uri() . '/assets/js/main.js', false, null, true);
   //wp_enqueue_script('jquery');
   //wp_enqueue_script('modernizr');
-  wp_enqueue_script('roots_plugins');
+//  wp_enqueue_script('roots_plugins');
   //wp_enqueue_script('roots_main');
 }
 add_action('wp_enqueue_scripts', 'roots_scripts', 100);
