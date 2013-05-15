434933e4148d982d836db1da6b2ce334d188f63a
arne.debree <arne.debree@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-05-15 07:51:44 +0000

    placed /login on login button

--- wp-content/themes/bidx/templates/content-intro.php
+++ wp-content/themes/bidx/templates/content-intro.php
@@ -7,47 +7,47 @@
 			<div class="hero">
 				<div class="pay-off ">
 					<div class="slider slide1">
-						<h1>Create your business group online 
+						<h1>Create your business group online
 							<span>Start connecting entrepreneurs with investors - today</span>
 						</h1>
-					
+
 					</div>
 					<div class="slider-nav">
 						<ul>
 							<li class="active"></li>
 							<li></li>
 							<li></li>
-							
+
 						</ul>
 					</div>
 				</div>
-				
+
 				<div class="signup">
 					<!-- ##### Widget Signup Start ######-->
 					<form class="fieldset">
 						<div><!-- this div is necessary for container overflow issue IE7 -->
 							<div class="title">Start your free 3 week trial today</div>
-						
+
 							<div class="formfield" data-validation='{"required":{"text":"This field is mandatory"},"typecheck":[{"custom":{"url":"/wp-admin/admin-ajax.php?action=bidx_request","apiurl":"groups/validateGroupName","apimethod":"get"}}]}'>
 							<!-- 	  <div class="formfield" data-validation='{"required":{"text":"This field is mandatory"},"custom":{"url":"/wp-admin/admin-ajax.php?action=bidx_request","apiurl":"groups/validateGroupName","apimethod":"get"}}'> -->
 								<input type="text" name="groupName" placeholder="Your group name" class="highlight" value="">
 							</div>
-														
+
 							<!-- <div class="formfield" data-validation='{"required":{"text":"This fields is mandatory"}}'> -->
 							<div class="formfield" data-validation='{"required":{"text":"This fields is mandatory"},"typecheck":[{"email":{"text":"This is not a valid e-mail address"}},{"custom":{"url":"/wp-admin/admin-ajax.php?action=bidx_request","apiurl":"members/validateUsername","apimethod":"get"}}]}'>
 								<!-- <div class="formfield" data-validation='{"required":{"text":"This fields is mandatory"},"typecheck":[{"email":{"text":"This is not a valid e-mail address"}},{"custom":{"url":"/wp-admin/admin-ajax.php?action=bidx_request","apiurl":"members/validateUsername","apimethod":"get"}}]}'> -->
-								
+
 								<input type="email" name="username" placeholder="Your email address">
 							</div>
-					
+
 							<div class="formfield" data-validation='{"required":{"text":"This fields is mandatory"}}'>
 								<input type="password" name="password" placeholder="Your password">
-								
+
 							</div>
 							<a href="#" class="button primary jsCreateGroup">Create your group</a>
 							<div class="login">
 								Already registered your Group?
-								<a href="#" class="button secondary">Login</a>
+								<a href="/login" class="button secondary">Login</a>
 							</div>
 						</div>
 					</form>
@@ -98,7 +98,7 @@
 				    		(function() {
 				    			/*custom event to fix field positioning when validation kicks in*/
 				    			var fs = $(".fieldset");
-				    			
+
 				    			var startMargin = parseInt(fs.css("margin-top").replace("px",""));
  								//when change occurs count error messages for this fieldset an calculate margin correction
 					    		$(".fieldset :input").change(correctMargins);
@@ -120,22 +120,22 @@
 				    			}
 
 							})();
-						});				    		
-				    </script> 
+						});
+				    </script>
 					<!-- ##### Widget Signup End ######-->
 				</div>
 
 			</div>
 			<!-- <div class="shade-bottom"></div> -->
-			
+
 		</div><!-- end container -->
 	</div>
 	<div class="block-odd">
-		
+
 		<br/>
 		<br/>
 	</div>
-	
+
 	<?php add_action('wp_footer', 'addToFooter',200);
 
 		function addToFooter() {
