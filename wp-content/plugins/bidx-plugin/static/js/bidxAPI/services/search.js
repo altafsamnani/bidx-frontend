98f9080c20583e675bd8e346d2d13c626e05ff04
jaap.gorjup <jaap.gorjup@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-05-16 06:36:23 +0000

    Updated Search

---
+++ wp-content/plugins/bidx-plugin/static/js/bidxAPI/services/search.js
@@ -0,0 +1,40 @@
+/**
+ * @version 1.0
+ * @author adebree
+ */
+;(function( $ )
+{
+    var bidx    = window.bidx
+    ,   api     = bidx.api
+    ,   session = {}
+    ,   baseUrl = "/api/v1/search"
+    ,   params  = []
+    ;
+
+    search.fetch = function( params )
+    {
+        var method = "GET";
+
+        api._call(
+        {
+            method:         method
+        ,   groupDomain:    params.groupDomain
+        ,   baseUrl:        baseUrl
+        ,   success:        function( response, textStatus, jqXhr )
+            {
+                if ( response && response.data )
+                {
+                    response = response.data;
+                }
+
+                params.success( response, textStatus, jqXhr );
+            }
+        ,   error:          function( jqXhr, textStatus, errorThrown )
+            {
+                params.error( jqXhr, textStatus, errorThrown );
+            }
+        } );
+    };
+
+    api.session = session;
+} )( jQuery );
