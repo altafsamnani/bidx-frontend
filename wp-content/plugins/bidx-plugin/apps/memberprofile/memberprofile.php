afd96dbaf02b639167647f055484ff8bb0d4e896
arne.debree <arne.debree@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-05-22 13:56:20 +0000

    fixed / implemented file upload for member profile picture

--- wp-content/plugins/bidx-plugin/apps/memberprofile/memberprofile.php
+++ wp-content/plugins/bidx-plugin/apps/memberprofile/memberprofile.php
@@ -2,16 +2,16 @@
 require_once(BIDX_PLUGIN_DIR .'/../apps/common.php' );
 
 /**
- * 
+ *
  * @author Altaf Samnani
  * @version 1.0
  */
 class memberprofile {
 
-	static $deps = array( 'jquery', 'jqueryui', 'bootstrap', 'underscore', 'backbone', 'json2', 'bidx-fileupload', 'bidx-form', 'bidx-form-element', 'bidx-location', 'bidx-utils', 'bidx-country-autocomplete', 'bidx-api-core' );
-	
+	static $deps = array( 'jquery', 'jqueryui', 'bootstrap', 'underscore', 'backbone', 'json2', 'gmaps-places', 'bidx-fileupload', 'bidx-form', 'bidx-form-element', 'bidx-location', 'bidx-utils', 'bidx-country-autocomplete', 'bidx-api-core' );
+
  	public $scriptInject ;
- 	
+
  	/**
 	 * Constructor
 	 */
@@ -19,25 +19,25 @@ class memberprofile {
 
     $bidCommonObj = new BidxCommon();
     $this->scriptInject = $bidCommonObj->getScriptJs();
-  
+
     //$this->scriptInject = BidxCommon::checkSession();
     //$bidCommonObj->checkSession();
-   
+
 		add_action( 'wp_enqueue_scripts', array( &$this, 'register_memberprofile_bidx_ui_libs' ) );
-    add_action('wp_head', array(&$this,'addJsVariables' ) );   
-     
+    add_action('wp_head', array(&$this,'addJsVariables' ) );
+
    }
 
-   
+
    function addJsVariables() {
 
      echo $this->scriptInject;
    }
-	
+
 	/**
 	 * Register scripts and styles
 	 */
-	function register_memberprofile_bidx_ui_libs() {		
+	function register_memberprofile_bidx_ui_libs() {
 	  wp_register_script( 'memberprofile', plugins_url( 'static/js/memberprofile.js', __FILE__ ), self :: $deps, '20130501', TRUE );
 	  wp_register_style( 'memberprofile', plugins_url( 'static/css/memberprofile.css', __FILE__ ), array(), '20130501', 'all' );
 	  wp_enqueue_style( 'memberprofile' );
@@ -68,7 +68,7 @@ echo "<script>
 	 */
 	function load($atts) {
 
-   
+
     /* 1 Template Rendering */
      require_once(BIDX_PLUGIN_DIR .'/templatelibrary.php');
      $view = new TemplateLibrary(BIDX_PLUGIN_DIR.'/memberprofile/templates/');
@@ -76,10 +76,10 @@ echo "<script>
     /* 2. Service MemberProfile*/
     require_once( BIDX_PLUGIN_DIR .'/../services/member-service.php' );
     $memberObj = new MemberService( );
-    
+
     /* 3. Render Member Profile Services for Initial View Display */
     $memberData = $memberObj->getMemberDetails(  );
-   
+
     $view->data = $memberData->data;
     $view->bidxGroupDomain = $memberData->bidxGroupDomain;
     $view->sessionData = BidxCommon::$staticSession;
@@ -88,5 +88,5 @@ echo "<script>
 		require_once ( BIDX_PLUGIN_DIR . '/memberprofile/memberprofile_component.php' );
 	}
 
-  
+
 }
