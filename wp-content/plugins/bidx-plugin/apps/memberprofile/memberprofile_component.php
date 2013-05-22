ee3985d65f702f8da14275f2da99d1b0d6016fb8
arne.debree <arne.debree@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-05-22 06:13:48 +0000

    btn-group the editControls

--- wp-content/plugins/bidx-plugin/apps/memberprofile/memberprofile_component.php
+++ wp-content/plugins/bidx-plugin/apps/memberprofile/memberprofile_component.php
@@ -11,7 +11,7 @@
     <div class="pageHeader clearfix">
       <div class="container clearfix">
         <h1 class="pull-left"><?php echo ($view->data->isMyProfile) ? 'My Profile' : ucwords($view->data->member->displayName); ?></h1>
-        <div class="editControls"></div>
+        <div class="editControls btn-group"></div>
       </div>
     </div>
 
@@ -21,7 +21,7 @@
         <?php $view->render('view-entrepreneur.phtml'); ?>
       </div>
       <div class="span4">
-        
+
         <?php $view->render('view-group.phtml'); ?>
       </div>
     </div>
