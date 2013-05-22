3979ae64eb1927e1179c0f0e8e1565a6713f66df
jaap.gorjup <jaap.gorjup@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-05-22 16:06:05 +0000

    Content added Theme support for dynamic information

--- wp-content/themes/bidx-group/templates/content-intro.php
+++ wp-content/themes/bidx-group/templates/content-intro.php
@@ -1,14 +1,18 @@
-	<!-- content starting here -->
-	<div class="block-even">
-		<div class="top-border"></div>
-		<div class="bottom-border"></div>
-		<div class="container">
-			<h1>GROUP HOMEPAGE</H1>
-			
-			
-		</div><!-- end container -->
-	</div>
+<div class="container">
+	<article class="bidx type-bidx status-publish hentry">
+	    <header>
 	
-	
-
-
+	    </header>
+	    <div class="entry-content">
+			<div class="well>
+				<h4>News and Events</h4>
+			</div>
+		</div>
+	    <div class="entry-content">
+		<?php echo do_shortcode( '[bidx app="group" view="lastMembers"]' ); ?>
+		</div>
+	    <div class="entry-content">
+		<?php echo do_shortcode( '[bidx app="group" view="lastMembers"]' ); ?>
+		</div>
+	</article>
+</div>
--- wp-content/themes/bidx-group/templates/footer-authenticated.php
+++ wp-content/themes/bidx-group/templates/footer-authenticated.php
@@ -1,15 +1,30 @@
 <footer>
-	<div class="container">
-		DASHBOARD FOOTER
+<div id="fb-root"></div>
+
+	<div class="sub-footer social-footer">
+		<ul>
+			<li>
+				Have a question?
+			</li>
+			<li>
+				Facebook
+			</li>
+			<li>
+				LinkedIn
+			</li>
+			<li>
+				Twitter
+			</li>
+		</ul>
 	</div>
 	<div class="sub-footer">
-			<ul>
-				<li>@ 2013. bidX.net. All rights reserved</li>
-				<li><a href="#">Terms of Service</a></li>
-				<li><a href="#">Privacy</a></li>
-				
-			</ul>
-		</div>
+		<ul>
+			<li>@ 2013. bidX.net. All rights reserved</li>
+			<li><a href="#">Terms of Service</a></li>
+			<li><a href="#">Privacy</a></li>
+			
+		</ul>
+	</div>
 </footer>
 <?php wp_footer(); ?><!-- this includes the script libraries defined in the lib/script file -->
 
