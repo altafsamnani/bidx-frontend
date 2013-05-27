04e211b32ad67258d744602a547f3462bed293ef
arne.debree <arne.debree@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-05-27 12:03:16 +0000

    do not show navigation on regstration step

--- wp-content/themes/bidx-group/templates/header.php
+++ wp-content/themes/bidx-group/templates/header.php
@@ -1,3 +1,7 @@
+<?php
+    $registration=preg_match( '/^\/registration/', $_SERVER[ 'REQUEST_URI' ] );
+?>
+
 <header class="banner navbar navbar-fixed-top" role="banner">
   	<div class="navbar-inner">
 		<div class="container">
@@ -5,6 +9,7 @@
 			<div class="clearfix">
 				<?php echo do_shortcode( '[bidx app="group" view="groupHeader"]' );?>
 
+<?php if ( !$registration ): ?>
 				<div class="pull-right">
 					<div class="topNav clearfix">
 						<form class="navbar-search" method="post" action="/search">
@@ -13,8 +18,10 @@
 				    	</form>
 					</div>
 				</div>
+<?php endif; ?>
 			</div>
 
+<?php if ( !$registration ): ?>
 			<nav class="nav-main mainNav" role="navigation">
 				<?php
 					if (has_nav_menu('group_dashboard_navigation')) :
@@ -22,6 +29,7 @@
 					endif;
 				?>
 			</nav>
+<?php endif; ?>
 
 		</div>
 	</div>
