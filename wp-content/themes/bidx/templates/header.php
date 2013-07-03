33de052de77db30e9e6dce911099085e4d3f67c3
mattijs.spierings <mattijs.spierings@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-07-03 14:18:44 +0000

    update styles and intro page responsive design

--- wp-content/themes/bidx/templates/header.php
+++ wp-content/themes/bidx/templates/header.php
@@ -1,19 +1,18 @@
 <header>
 	<div class="container">
 		<div class="navbar">
-			<div class="container">
-				<a class="btn btn-navbar collapsed" data-toggle="collapse" data-target=".nav-bidx-collapse">
-	<!-- 				<span class="icon-bar"></span>
-					<span class="icon-bar"></span>
-					<span class="icon-bar"></span> -->
-				</a>
-
+			<div class="container"> 
+				<button type="button" class="btn btn-navbar collapsed" data-toggle="collapse" data-target=".nav-collapse">
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+		            <span class="icon-bar"></span>
+          		</button>
 				<a class="logo" href="<?php echo home_url(); ?>/">
 					<img src="/wp-content/themes/bidx/assets/img/logo-desktop.png" class="hidden-phone">
 					<img src="/wp-content/themes/bidx/assets/img/logo-mobile.png" class="visible-phone">
 				</a>
 				
-				<nav class="menu-top visible-tablet visible-desktop">
+				<nav class="menu-top visible-desktop">
 					<ul class="unstyled inline nav">
 						<li><a href="/category/blog">Blog</a></li>
 						<li><a href="/about">About bidX</a></li>
@@ -21,7 +20,7 @@
 					</ul>
 				</nav>
 			</div>
-			<nav class="nav-bidx-collapse collapse menu-main">
+			<nav class="nav-collapse collapse menu-main">
 				<?php
 				if (has_nav_menu('primary_navigation')) :
 					wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav menu'));
