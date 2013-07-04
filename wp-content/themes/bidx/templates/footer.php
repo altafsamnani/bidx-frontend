c3bb8dfdf63334dddbdca75789a473715585a85e
mattijs.spierings <mattijs.spierings@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-07-04 12:40:46 +0000

    cleaning up styles bidx

--- wp-content/themes/bidx/templates/footer.php
+++ wp-content/themes/bidx/templates/footer.php
@@ -1,5 +1,5 @@
 <footer>
-<!--     <div class="partners">
+     <div class="partners">
         <div class="container partnerlist">
             <a href="http://www.bidnetwork.org" class="partner partner-bid">BiD Network</a>
             <a href="http://www.puterasampoerna.com/" class="partner partner-sampoerna">Putera Sampoerna</a>
@@ -8,28 +8,35 @@
             <a href="https://www.usaid.gov" class="partner partner-usaid">USAID</a>
         </div>
     </div>
- -->
+
 
     <div class="page-footer">
-    	<div class="pull-left social-links">
-    		<span>Follow us</span>
+        <div class="container">
+            <div class="footer-bar">
+            	<div class="pull-left ">
+            		<span>Follow us</span>
 
-    		<ul class="unstyled inline">
-				<li><a href="https://twitter.com/bid_x"><span class="fui-twitter"></span></a></li>
-				<li><a href="https://www.facebook.com"><span class="fui-facebook"></span></a></li>
-				<li><a href="http://www.linkedin.com/company/bidx"><span class="fui-linkedin"></span></a></li>
-    		</ul>
-    	</div>
+            	 	<div class="inline-list social-links">
+                        <div><a href="https://twitter.com/bid_x"><span class="fui-twitter"></span></a></div>
+                        <div><a href="https://www.facebook.com"><span class="fui-facebook"></span></a></div>
+                        <div><a href="http://www.linkedin.com/company/bidx"><span class="fui-linkedin"></span></a></div>
+                    </div>
+        				<!-- <li><a href="https://twitter.com/bid_x"><span class="fui-twitter"></span></a></li>
+        				<li><a href="https://www.facebook.com"><span class="fui-facebook"></span></a></li>
+        				<li><a href="http://www.linkedin.com/company/bidx"><span class="fui-linkedin"></span></a></li> -->
+            		<!-- </ul> -->
+            	</div>
 
-    	<div class="pull-right">
-    		<span class="copyright">&copy; 2013. bidX.net. All rights reserved</span>
-
-    		<ul class="unstyled inline footer-menu">
-    			<li><a href="/sitemap">Sitemap</a></li>
-    			<li><a href="/termsandconditions">Terms and conditions</a></li>
-    			<li><a href="/privacy">Privacy</a></li>
-    		</ul>
-    	</div>
+            	<div class="pull-right">
+            		<span class="copyright">&copy; 2013. bidX.net. All rights reserved</span>
+                    <div class="inline-list footer-menu">
+            			<div><a href="/sitemap">Sitemap</a></div>
+            			<div><a href="/termsandconditions">Terms and conditions</a></div>
+            			<div><a href="/privacy">Privacy</a></div>
+            		</div>
+            	</div>
+            </div>
+        </div>
     </div>
 </footer>
 
