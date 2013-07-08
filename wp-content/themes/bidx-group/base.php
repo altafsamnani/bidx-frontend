7ff3a2df365f53f630f8bd8a1b7aa79ca2c7bb8b
mattijs.spierings <mattijs.spierings@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-07-08 12:25:04 +0000

    update of group page

--- wp-content/themes/bidx-group/base.php
+++ wp-content/themes/bidx-group/base.php
@@ -1,7 +1,7 @@
 <?php get_template_part('templates/head'); ?>
 <?php include_once("lib/dBug.php")?>
 <body <?php body_class(); ?>>
-
+<div id="wrap"> <!-- sticky footer -->
 <!--[if lt IE 7]><div class="alert">Your browser is <em>ancient!</em> <a href="http://browsehappy.com/">Upgrade to a different browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">install Google Chrome Frame</a> to experience this site.</div><![endif]-->
 
 <?php
@@ -19,14 +19,17 @@
     }
 
     include roots_template_path();
+?>
+    <div id="push"></div>
+</div><!-- sticky footer -->
 
+<?php
     if($authenticated) {
       get_template_part('templates/footer-authenticated');
     }
     else {
-		  get_template_part('templates/footer');
+          get_template_part('templates/footer');
     }
 ?>
-
 </body>
 </html>
