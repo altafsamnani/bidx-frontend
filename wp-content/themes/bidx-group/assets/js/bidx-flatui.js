a355b6fcf1a48915de1c686553b53e609b6420dd
arne <arne@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-07-12 11:29:03 +0000

    swapped input-append/prepend focus application to delegates because of snippets

--- wp-content/themes/bidx-group/assets/js/bidx-flatui.js
+++ wp-content/themes/bidx-group/assets/js/bidx-flatui.js
@@ -1,11 +1,20 @@
 // This file basically contains some snippets found / used by FlatUI to make.. euh.. FlatUI work.
 //
-(function($) {
+( function( $ )
+{
 
-    // Focus state for append/prepend inputs
-    $('.input-prepend, .input-append').on('focus', 'input', function () {
-      $(this).closest('.control-group, form').addClass('focus');
-    }).on('blur', 'input', function () {
-      $(this).closest('.control-group, form').removeClass('focus');
-    });
+    var $body = $( "body" );
+
+    // Focus state for append/prepend inputs. Use of delegate because of snippets
+    //
+    $body.delegate( ".input-prepend input, .input-append input", "focus", function()
+    {
+        $(this).closest('.control-group, form').addClass('focus');
+    } );
+
+    $body.delegate( ".input-prepend input, .input-append input", "blur", function()
+    {
+        $(this).closest('.control-group, form').removeClass('focus');
+    } );
+
 } ( jQuery ));
