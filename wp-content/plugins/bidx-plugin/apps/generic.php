45df274a7250431ca6e27dd3551bec3a5cb1c3a4
altaf.samnani <altaf.samnani@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-07-17 08:50:11 +0000

    correcting variable

--- wp-content/plugins/bidx-plugin/apps/generic.php
+++ wp-content/plugins/bidx-plugin/apps/generic.php
@@ -23,7 +23,7 @@ class Generic
                 break;
 
             case 'company' :
-                $dep[] = array ('jquery', 'jquery-ui', 'bootstrap', 'underscore', 'backbone', 'json2','gmaps-places', 'holder', 'bidx-form', 'bidx-utils', 'bidx-api-core', 'bidx-common', 'bidx-data' ,'businessplan');
+                $dep = array ('jquery', 'jquery-ui', 'bootstrap', 'underscore', 'backbone', 'json2','gmaps-places', 'holder', 'bidx-form', 'bidx-utils', 'bidx-api-core', 'bidx-common', 'bidx-data' ,'businessplan');
                 if (strpos ($uri, 'business')) {
                     $dep[] = 'businessplan';                    
                 }
