93134a184e56ab4d27b9d1582bbb96c5ef8eb935
arne <arne@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-08-19 13:03:50 +0000

    cleaned up some old code in company.php

--- wp-content/plugins/bidx-plugin/apps/company/company.php
+++ wp-content/plugins/bidx-plugin/apps/company/company.php
@@ -17,11 +17,6 @@ class company extends Generic
      */
     function __construct ()
     {
-        //$subDomain = $this->getBidxSubdomain ();
-
-       // $bidCommonObj = new BidxCommon ($subDomain);
-        //$this->scriptInject = $bidCommonObj->getScriptJs ($subDomain);
-
         add_action ('wp_enqueue_scripts', array (&$this, 'register_company_bidx_ui_libs'));
     }
 
@@ -34,33 +29,6 @@ class company extends Generic
         wp_register_script ('company', plugins_url ('static/js/company.js', __FILE__), $deps, '20130501', TRUE);
         wp_register_style ('company', plugins_url ('static/css/company.css', __FILE__), array (), '20130501', 'all');
         wp_enqueue_style ('company');
-    }
-
-    /**
-     * Grab the subdomain portion of the URL. If there is no sub-domain, the root
-     * domain is passed back. By default, this function *returns* the value as a
-     * string.
-     *
-     * @param bool $echo optional parameter prints the response directly to
-     * the screen.
-     */
-    function getBidxSubdomain ($echo = false)
-    {
-        $hostAddress = explode ('.', $_SERVER ["HTTP_HOST"]);
-        if (is_array ($hostAddress)) {
-            if (eregi ("^www$", $hostAddress [0])) {
-                $passBack = 1;
-            } else {
-                $passBack = 0;
-            }
-            if ($echo == false) {
-                return ( $hostAddress [$passBack] );
-            } else {
-                echo ( $hostAddress [$passBack] );
-            }
-        } else {
-            return ( false );
-        }
     }
 
     /**
