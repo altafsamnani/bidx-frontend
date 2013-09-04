76719081c335a8a918bd9b5d5c686f52ac0e46c5
mattijs.spierings <mattijs.spierings@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-09-04 10:54:32 +0000

    fixed carousel in group theme

--- wp-content/themes/bidx-group-template/templates/content-intro.php
+++ wp-content/themes/bidx-group-template/templates/content-intro.php
@@ -1,213 +1,227 @@
 <?php
 
-$sessionData = BidxCommon::$staticSession;
-$entities = $sessionData->data->wp->entities;
+    $sessionData = BidxCommon::$staticSession;
+    $entities = $sessionData->data->wp->entities;
 
-$authenticated=false;
-if ($sessionData->authenticated == 'true') {
-  $authenticated=true;
-}
-
+    $authenticated=false;
+    if ($sessionData->authenticated == 'true')
+    {
+        $authenticated=true;
+    }
 ?>
-<div class="container outer-content-container">
-	<article class="bidx type-bidx status-publish hentry">
-        <?php echo bidx_get_status_msgs(); ?>
-	    <header>
-            <div class="row-fluid">
-                <div class="span<?php echo $authenticated ? '9' : '12'; ?>">
-        			<div id="myCarousel" class="carousel slide">
-        			<?php
-        						$images = get_children(
-        									array(
-        									'post_parent' => $post->ID,
-        									'post_status' => 'inherit',
-        									'post_type' => 'attachment',
-        									'post_mime_type' => 'image',
-        									'order' => 'ASC',
-        									'orderby' => 'menu_order' )
-        									);
-        						if ( count( $images ) > 0 ) {
-        						?>
-        						<ol class="carousel-indicators">
-        						<?php
-        							$count = 0;
-        							foreach ( $images as $id => $image ) { ?>
-        							<li data-target="#introCarousel" data-slide-to="<?php echo $count ?>"
-        							<?php if ($count == 0) { ?> class="active" <?php } $count++; ?>></li>
-        						<?php } ?>
-        						</ol>
-        						<!-- Carousel items -->
-        						<div class="carousel-inner">
-        						<?php
-        							$count = 0;
-        							foreach ( $images as $id => $image ) {
-        								$img = wp_get_attachment_image_src( $image->ID, 400 );
-        						?>
-        							<div class="item <?php if ($count == 0) { ?>active<?php } $count++; ?>">
-        								<img class="img-rounded" src="<?php echo $img[0] ?>" />
-        							    <div class="carousel-caption img-rounded">
-        			                      <h4><?php echo $image -> post_title ?></h4>
-        			                      <p><?php echo $image -> post_content ?></p>
-        			                    </div>
-        							</div>
-        						<?php }
-        						} else {
-        						?>
-        			  <ol class="carousel-indicators">
-        			    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-        			    <li data-target="#myCarousel" data-slide-to="1"></li>
-        			    <li data-target="#myCarousel" data-slide-to="2"></li>
-        			  </ol>
-        			  <!-- Carousel items -->
-        			  <div class="carousel-inner">
-        			    <div class="active item">
-        			    	<img src="/wp-content/themes/bidx-group/assets/img/sample/you_should_be_here.jpg" />
-        			    	<div class="carousel-caption">
-                              <h4>First Thumbnail label</h4>
-                              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            </div>
-        			    </div>
-        			    <div class="item">
-        			    	<img src="/wp-content/themes/bidx-group/assets/img/sample/birds_eye_la_defense.jpg" />
-        			    	<div class="carousel-caption">
-                              <h4>Second Thumbnail label</h4>
-                              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            </div>
-        			    </div>
-        			    <div class="item">
-        			    	<img src="/wp-content/themes/bidx-group/assets/img/sample/alien_entrepreneurs.jpg" />
-        			    	<div class="carousel-caption">
-                              <h4>Third Thumbnail label</h4>
-                              <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                            </div>
-        			    </div>
-        			  </div>
-        	           <?php  } // $images > 0 ?>
-        			</div><!-- // carousel -->
-                </div><!-- span -->
+
+
+<div class="outer-content-container">
+
+<?php echo bidx_get_status_msgs(); ?>
+
+    <!-- IMAGE Carousel -->
+    <div id="myCarousel" class="carousel slide">
 <?php
-    if ( $authenticated ) {
-?>
-                <div class="span3">
-<?php
+    $images = get_children(
+    array(
+    'post_parent' => $post->ID,
+    'post_status' => 'inherit',
+    'post_type' => 'attachment',
+    'post_mime_type' => 'image',
+    'order' => 'ASC',
+    'orderby' => 'menu_order' )
+    );
 
-    // Create investor
-    //
-    if ( !isset( $entities->bidxInvestorProfile )) {
+    if ( count( $images ) > 0 )
+    {
 ?>
-        <div class="well">
-            <h3><?php _e('Become an investor','bidxtheme')?></h3>
-            <div class="row-fluid">
-                <div class="span8">
-                    <p><?php _e('Create your business','bidxtheme')?></p>
-                </div>
-                <div class="span4">
-                    <a class="btn btn-primary" href="/member/#createInvestor"><?php _e('Create','bidxtheme')?></a>
-                </div>
+        <div class="outer-carousel-control-container">
+            <div class="container">
+                <ol class="carousel-indicators">
+<?php
+                $count = 0;
+                foreach ( $images as $id => $image )
+                {
+?>
+                    <li data-target="#myCarousel" data-slide-to="<?php echo $count ?>" <?php if ($count == 0) { ?>class="active" <?php } $count++; ?> ></li>
+<?php
+                }
+?>
+                </ol>
+
+                <!-- Carousel nav -->
+                <a class="carousel-control left" href="#myCarousel" data-slide="prev">&lsaquo;</a>
+                <a class="carousel-control right" href="#myCarousel" data-slide="next">&rsaquo;</a>
+
             </div>
         </div>
+        <!-- Carousel items -->
+        <div class="carousel-inner">
 <?php
-    }
-
-    // Create entrepreneur
-    //
-    if ( !isset( $entities->bidxEntrepreneurProfile )) {
+        $count = 0;
+        foreach ( $images as $id => $image )
+        {
+            $img = wp_get_attachment_image_src( $image->ID, 400 );
 ?>
-        <div class="well">
-            <h3><?php _e('Become an entrepreneur')?></h3>
-            <div class="row-fluid">
-                <div class="span8">
-                    <p><?php _e('Create your business')?></p>
-                </div>
-                <div class="span4">
-                    <a class="btn btn-primary" href="/member/#createEntrepreneur"><?php _e('Create','bidxtheme')?></a>
+            <div class="item <?php if ($count == 0) { ?>active<?php } $count++; ?>">
+                <img class="img-rounded" src="<?php echo $img[0] ?>" />
+                <div class="carousel-caption img-rounded">
+                    <h4><?php echo $image -> post_title ?></h4>
+                    <p><?php echo $image -> post_content ?></p>
                 </div>
             </div>
+<?php
+        }
+
+    } // $images > 0
+?>
         </div>
+
+
+    </div>
+    <!-- // IMAGE Carousel -->
+
+
+	<div class="container">
+<?php
+        if ( $authenticated )
+        {
+?>
+            <div class="span3">
 <?php
 
-    }
-
+            // Create investor
+            //
+            if ( !isset( $entities->bidxInvestorProfile ))
+            {
 ?>
+                <div class="well">
+                    <h3><?php _e('Become an investor','bidxtheme')?></h3>
+                    <div class="row-fluid">
+                        <div class="span8">
+                            <p><?php _e('Create your business','bidxtheme')?></p>
+                        </div>
+                        <div class="span4">
+                            <a class="btn btn-primary" href="/member/#createInvestor"><?php _e('Create','bidxtheme')?></a>
+                        </div>
+                    </div>
                 </div>
 <?php
+            }
+
+            // Create entrepreneur
+            //
+            if ( !isset( $entities->bidxEntrepreneurProfile ))
+            {
+?>
+                <div class="well">
+                    <h3><?php _e('Become an entrepreneur')?></h3>
+                    <div class="row-fluid">
+                        <div class="span8">
+                            <p><?php _e('Create your business')?></p>
+                        </div>
+                        <div class="span4">
+                            <a class="btn btn-primary" href="/member/#createEntrepreneur"><?php _e('Create','bidxtheme')?></a>
+                        </div>
+                    </div>
+                </div>
+<?php
+            }
+
+?>
+            </div>
+<?php
+        }
+?>
+
+
+
+<?php
+        if ( ! $authenticated )
+        {
+?>
+    	    <div class="entry-content">
+    	    	<?php echo do_shortcode( '[bidx app="group" view="group-intro"]' ); ?>
+        	</div>
+        	<div class="entry-content">
+        		<div class="row-fluid after-well" >
+    	    		<div class="span12 text-center">
+    	    			<a class="btn" href="/login?join=true"><?php _e('Join our group','bidxtheme')?></a> <?php printf(__('Already have an account? Just %1$s','bidxtheme'),'<a href="/login">login</a>');?>
+    	    		</div>
+        		</div>
+        	</div>
+<?php
+        }
+        else
+        {
+            // Logged in but not joined this group
+            //
+?>
+            <div class="entry-content">
+                <?php echo do_shortcode( '[bidx app="group" view="join-group"]' ); ?>
+            </div>
+<?php
     }
 ?>
 
-            </div><!-- row -->
-	    </header>
-<?php
-    if ( ! $authenticated ) {
-?>
-	    <div class="entry-content">
-	    	<?php echo do_shortcode( '[bidx app="group" view="group-intro"]' ); ?>
-    	</div>
-    	<div class="entry-content">
-    		<div class="row-fluid after-well" >
-	    		<div class="span12 text-center">
-	    			<a class="btn" href="/login?join=true"><?php _e('Join our group','bidxtheme')?></a> <?php printf(__('Already have an account? Just %1$s','bidxtheme'),'<a href="/login">login</a>');?>
-	    		</div>
-    		</div>
-    	</div>
-<?php } else {
-    // Logged in but not joined this group
-    //
-?>
+
         <div class="entry-content">
-            <?php echo do_shortcode( '[bidx app="group" view="join-group"]' ); ?>
+        	<div class="row-fluid">
+    			<div class="well">
+    				<h4><?php _e('News and Events','bidxtheme')?></h4>
+    				<div class="row-fluid">
+<?php
+                    $args=array(
+                      'post_type' => 'post',
+                      'post_status' => 'publish',
+                      'posts_per_page' => 2
+                      );
+                    $my_query = null;
+                    $my_query = new WP_Query($args);
+
+                    if( $my_query->have_posts() )
+                    {
+                        while ( $my_query->have_posts() )
+                        {
+                            $my_query->the_post();
+?>
+                			<div class="span6 ">
+                				<div class="media member-thumb img-rounded">
+                  					<a class="pull-left" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
+<?php
+                					if ( has_post_thumbnail() )
+                                    {
+                  						$size = array(100,100);
+                  						$atts = array('data-src' => 'holder.js/50x50/social/auto/text:News',
+                  									  'class' => 'media-object img-rounded');
+                  						the_post_thumbnail($size, $atts);
+                  					}
+                  					else
+                                    {
+?>
+	       					          <img data-src="holder.js/50x50/social/auto/text:None" class="media-object img-rounded" />
+<?php
+                                    }
+?>
+                					</a>
+                					<div class="media-body centering">
+                						<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
+                						<p><?php the_excerpt(); ?></p>
+                					</div>
+                				</div>
+                			</div>
+<?php
+                        }
+                    }
+                    wp_reset_query();
+?>
+                    </div>
+                </div>
+            </div>
         </div>
-<?php
-    }
-?>
 
+        <div class="entry-content group-members">
+            <?php echo do_shortcode( '[bidx app="group" view="last-members"]' ); ?>
+        </div>
 
-	    <div class="entry-content">
-	    	<div class="row-fluid">
-				<div class="well">
-					<h4><?php _e('News and Events','bidxtheme')?></h4>
-					<div class="row-fluid">
-<?php
- $args=array(
-      'post_type' => 'post',
-      'post_status' => 'publish',
-      'posts_per_page' => 2
-      );
-    $my_query = null;
-    $my_query = new WP_Query($args);
-    if( $my_query->have_posts() ) {
-      while ($my_query->have_posts()) : $my_query->the_post(); ?>
-					<div class="span6 ">
-						<div class="media member-thumb img-rounded">
-		  					<a class="pull-left" href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>">
-		  					<?php
-		  						if ( has_post_thumbnail() ) {
-			  						$size = array(100,100);
-			  						$atts = array('data-src' => 'holder.js/50x50/social/auto/text:News',
-			  									  'class' => 'media-object img-rounded');
-			  						the_post_thumbnail($size, $atts);
-			  					}
-			  					else {	?>
-								<img data-src="holder.js/50x50/social/auto/text:None" class="media-object img-rounded" />
-					<?php	} ?>
-							</a>
-							<div class="media-body centering">
-   								<h4><a href="<?php the_permalink() ?>"><?php the_title(); ?></a></h4>
-								<p><?php the_excerpt(); ?></p>
-							</div>
-						</div>
-					</div><?php
-       endwhile;
-    }
-	wp_reset_query();
-?>
-					</div>
-				</div>
-			</div>
-		</div>
+    </div>
+    <!-- // container -->
 
-	    <div class="entry-content group-members">
-		<?php echo do_shortcode( '[bidx app="group" view="last-members"]' ); ?>
-		</div>
-
-	</article>
 </div>
+<!-- // outer-content-container -->
--- wp-content/themes/bidx-group-template/templates/header.php
+++ wp-content/themes/bidx-group-template/templates/header.php
@@ -3,29 +3,29 @@
 		<div class="container">
 						
 			<div class="row-fluid">
-			<?php
+<?php
 				if( $authenticated )
 				{
-			?>
+?>
 				<div class="iconbar-wrapper">
 					<?php echo do_shortcode( '[bidx app="group" view="navbar"]' );?>
 				</div>
-			<?php
+<?php
 				}
 				// the group-header is displayed for authenticated and non-authenticated
 				echo do_shortcode( '[bidx app="group" view="group-header"]' );
 	
 				if( $authenticated )
 				{
-			?>
+?>
 				<div class="search">
 			 		<?php get_template_part('templates/searchform'); ?>
 				</div>
-			<?php 
+<?php 
 				}
 				if( !$authenticated )
 				{
-			 ?>
+?>
 	 			<div class="pull-right span4">
 					<div class="row-fluid">
 						<div class="span6">
@@ -37,21 +37,20 @@
 
 					</div>
 				</div>
-			 <?php 
+<?php 
 			 	} 
-			 ?>
+?>
 
-			<nav class="nav-collapse collapse menu-main">
-				<?php
-					if (has_nav_menu('group_dashboard_navigation')) :
-						wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav'));
-					endif;
-				?>
-			</nav>
-	
-			
-
+				<nav class="nav-collapse collapse menu-main">
+<?php
+						if (has_nav_menu('group_dashboard_navigation')) :
+							wp_nav_menu(array('theme_location' => 'primary_navigation', 'menu_class' => 'nav'));
+						endif;
+?>
+				</nav>
+			</div>
 		</div>
 	</div>
+	<div class="white-spacer"></div>
 
 </header>
