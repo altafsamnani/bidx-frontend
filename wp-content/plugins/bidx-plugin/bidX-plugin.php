2ba0fef7351ef5c4cfc8fa2225e48dc4df3a947c
altaf.samnani <altaf.samnani@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-09-18 16:15:51 +0000

    again

--- wp-content/plugins/bidx-plugin/bidX-plugin.php
+++ wp-content/plugins/bidx-plugin/bidX-plugin.php
@@ -1,21 +1,24 @@
 <?php
+
 /*
-Plugin Name: bidx-plugin
-Plugin URI: http://bidx.net/plugin/bidx-plugin
-Description: Wordpress plugin for adding bidX functionality to a website.
-Version: 0.1.2
-Author: bidX development team
-Author URI: http://bidx.net/plugin/bidx-plugin
-License: Commercial
-*/
+  Plugin Name: bidx-plugin
+  Plugin URI: http://bidx.net/plugin/bidx-plugin
+  Description: Wordpress plugin for adding bidX functionality to a website.
+  Version: 0.1.2
+  Author: bidX development team
+  Author URI: http://bidx.net/plugin/bidx-plugin
+  License: Commercial
+ */
 
 //
 //Generic config for paths
 //
 
+/* If Network activate it performs action on Main site too, exclude that site */
+global $blog_id;
+if ($blog_id != 1) {
 
-if( $blog_id != 1) {
-include ('apps/util.php' );
+    include ('apps/util.php' );
 
 
 
@@ -23,52 +26,46 @@ include ('apps/util.php' );
 // Activation of frontend apps
 //
 //require ( BIDX_PLUGIN_DIR . '/rewrite.php' ); //to be removed
-require ( BIDX_PLUGIN_DIR . '/contentloader.php' );
-require ( BIDX_PLUGIN_DIR . '/shortcode.php' );
-require ( BIDX_PLUGIN_DIR . '/templatelibrary.php' );
-require_once ( BIDX_PLUGIN_DIR . '/common.php' );
+    require ( BIDX_PLUGIN_DIR . '/contentloader.php' );
+    require ( BIDX_PLUGIN_DIR . '/shortcode.php' );
+    require ( BIDX_PLUGIN_DIR . '/templatelibrary.php' );
+    require_once ( BIDX_PLUGIN_DIR . '/common.php' );
 
 
 
 //do not do this when site is bidx.net
-$loader = new ContentLoader( 'pages' );
-register_activation_hook( __FILE__, array( $loader, 'load' ) );
-register_deactivation_hook( __FILE__, array( $loader, 'unload' ) );
+    $loader = new ContentLoader ('pages');
+    register_activation_hook (__FILE__, array ($loader, 'load'));
+    register_deactivation_hook (__FILE__, array ($loader, 'unload'));
 
-if ( !BidxCommon :: isWPInternalFunction() ) {
-	//Mattijs 8/7/2013: would be nice if this array is populated from XML so you dont have to define your apps twice
-	$ruleitems = array(
-			'auth',
-			'businessplan',
-			'businesssummary',
-			'company',
-			'group',
-			'mail',
-			'media',
-			'member',
-			'mydashboard' => 'dashboard',
-			'search'
-	);
-	$shortcode = new BidxShortCode();
-	$shortcode -> addMappingArray( $ruleitems );
+    if (!BidxCommon :: isWPInternalFunction ()) {
+        //Mattijs 8/7/2013: would be nice if this array is populated from XML so you dont have to define your apps twice
+        $ruleitems = array (
+          'auth',
+          'businessplan',
+          'businesssummary',
+          'company',
+          'group',
+          'mail',
+          'media',
+          'member',
+          'mydashboard' => 'dashboard',
+          'search'
+        );
+        $shortcode = new BidxShortCode();
+        $shortcode->addMappingArray ($ruleitems);
 
-	//cleanup body tag
-
-}
+        //cleanup body tag
+    }
 //Bidx WP Hooks file
-require_once( BIDX_PLUGIN_DIR .'/../services/wp-service.php' );
-
+    require_once( BIDX_PLUGIN_DIR . '/../services/wp-service.php' );
 }
-add_filter( 'body_class','my_class_names', 100000 );
+add_filter ('body_class', 'my_class_names', 100000);
 
-function my_class_names($classes) {
+function my_class_names ($classes)
+{
 
-	return preg_replace( "/^\?bidx=/", 'bidx-',  $classes );
-
+    return preg_replace ("/^\?bidx=/", 'bidx-', $classes);
 }
-
-
-
-
 
 ?>
