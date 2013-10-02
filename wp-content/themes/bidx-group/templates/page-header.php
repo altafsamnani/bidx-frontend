e094392fdf249f3cf34c023f41feadf292aec71e
mattijs.spierings <mattijs.spierings@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-10-02 07:56:37 +0000

    removed old theme

--- wp-content/themes/bidx-group/templates/page-header.php
+++
@@ -1,7 +0,0 @@
-<!-- 
-<div class="">
-	  <h1>
-	    <?php echo roots_title(); ?>
-	  </h1>
-	</div>
--->
--- wp-content/themes/bidx-group/templates/searchform.php
+++
@@ -1,6 +0,0 @@
-<form class="form-search" action="/search" method="GET">
-    <div class="input-append">
-        <input type="text" class="search-query search-query-rounded" name="q" placeholder="Search" value="<?php echo isset( $_REQUEST[ 'q' ] ) ? $_REQUEST['q'] : ''; ?>">
-        <button type="submit" class="btn"><span class="fui-search"></span></button>
-    </div>
-</form>
