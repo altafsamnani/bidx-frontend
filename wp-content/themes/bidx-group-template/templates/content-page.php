a74bb15ddacf52feabc08d810dd80e739b99b338
sakis.katsikas <sakis.katsikas@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-10-04 16:04:22 +0000

    Added page and post templates, Added category list

---
+++ wp-content/themes/bidx-group-template/templates/content-page.php
@@ -0,0 +1,7 @@
+<?php while (have_posts()) : the_post(); ?>
+	<br>
+	<div class="container">
+		<?php the_content(); ?>
+		<?php wp_link_pages(array('before' => '<nav class="pagination">', 'after' => '</nav>')); ?>
+	</div>
+<?php endwhile; ?>
--- wp-content/themes/bidx-group-template/templates/content-single.php
+++ wp-content/themes/bidx-group-template/templates/content-single.php
@@ -1,24 +1,56 @@
 <?php while (have_posts()) : the_post(); ?>
+<br>
 <div class="container">
-	<article <?php post_class(); ?>>
-    <header>
-      <h1 class="entry-title"><?php the_title(); ?></h1>
-      <?php get_template_part('templates/entry-meta'); ?>
-    </header>
-    <div class="entry-content">
+  <div class="row-fluid">
+    <div class="span8">
+    	<article <?php post_class(); ?>>
+        <header>
+          <h1 class="entry-title"><?php the_title(); ?></h1>
+          <hr>
+          <?php get_template_part('templates/entry-meta'); ?>
+        </header>
+        <div class="entry-content">
+          <?php the_content(); ?>
+        </div>
+        <footer>
 
-      <?php the_content(); ?>
+          <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
+          <?php the_tags('<ul class="entry-tags"><li>','</li><li>','</li></ul>'); ?>
 
-    </div>
-    <footer>
+        </footer>
+        <?php 
+        //  comments_template('/templates/comments.php'); 
+        ?>
+      </article>
+      </div> 
 
-      <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
-      <?php the_tags('<ul class="entry-tags"><li>','</li><li>','</li></ul>'); ?>
-
-    </footer>
-    <?php 
-    //	comments_template('/templates/comments.php'); 
-    ?>
-  </article>
- </div> 
 <?php endwhile; ?>
+
+<?php 
+
+    $category = get_the_category(); 
+
+    if ( $category ) { ?> 
+    <div class="span4 well">
+      <h4><?php echo $category[0]->name; ?></h4>
+    <?php
+
+      // The Query
+      query_posts( array ( 'category_name' => $category[0]->category_nicename, 'posts_per_page' => -1 ) );
+
+      // The Loop
+      while ( have_posts() ) : the_post(); ?>
+        <li>
+          <a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a>
+        </li>
+
+      <?php endwhile;
+
+      // Reset Query
+      wp_reset_query(); ?>
+      </div>
+<?php 
+  }
+?>
+    </div> 
+  </div> 
