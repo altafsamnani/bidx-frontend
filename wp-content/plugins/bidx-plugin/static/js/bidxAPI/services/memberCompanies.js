b522f009e512c96a1cb7bb5d712327733491f8be
arne <arne@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-10-14 08:35:52 +0000

    added memberCompanies bidxAPI service file (API doesn't exist yet thought)

---
+++ wp-content/plugins/bidx-plugin/static/js/bidxAPI/services/memberCompanies.js
@@ -0,0 +1,45 @@
+/**
+ * @version 1.0
+ * @author adebree
+ */
+;(function( $ )
+{
+    var bidx    = window.bidx
+    ,   api     = bidx.api
+    ,   service = {}
+    ,   baseUrl = "/api/v1/members/%memberId%/companies"
+    ,   params  = []
+    ;
+
+    service.fetch = function( params )
+    {
+        var method = "GET"
+        ,   url    = baseUrl.replace( "/%memberId%", params.memberId )
+        ;
+
+        api._call(
+        {
+            method:                     method
+        ,   groupDomain:                params.groupDomain
+        ,   baseUrl:                    url
+        ,   extraUrlParameters:         params.extraUrlParameters
+        ,   success:        function( response, textStatus, jqXhr )
+            {
+                if ( response && response.data )
+                {
+                    response = response.data;
+                }
+
+                params.success( response, textStatus, jqXhr );
+            }
+        ,   error:          function( jqXhr, textStatus, errorThrown )
+            {
+                params.error( jqXhr, textStatus, errorThrown );
+            }
+        } );
+    };
+
+    // Expose
+    //
+    api.memberRelationships = service;
+} )( jQuery );
