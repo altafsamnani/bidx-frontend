04db33c1b215d579f5471d70f76fa47e7a9da5aa
mattijs.spierings <mattijs.spierings@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-10-22 16:49:17 +0000

    implemented password change in Account

--- wp-content/plugins/bidx-plugin/apps/account/static/js/account.js
+++ wp-content/plugins/bidx-plugin/apps/account/static/js/account.js
@@ -1,8 +1,11 @@
 ;( function ( $ )
 {
-    var $element                    = $( "#register" )
-    ,   $frmRegister                = $element.find( "#frmRegister" )
-    ,   $btnRegister                = $frmRegister.find( ":submit" )
+    "use strict";
+
+    var $element                    = $( "#account" )
+    ,   $views                      = $element.find( ".view" )
+    ,   $frmCredentials             = $element.find( "#frmCredentials" )
+    ,   $btnUpdateCredentials       = $frmCredentials.find( ":submit" )
     ,   bidx                        = window.bidx
     ,   appName                     = "account"
     ,   location
@@ -10,40 +13,36 @@
 
     // private functions
 
-    var _oneTimeSetup = function()
+    function _oneTimeSetup()
     {
 
        // set validation and submitHandler
         //
-        $frmRegister.validate(
+        $frmCredentials.validate(
         {
             ignore: ":hidden"
         ,   debug:  false
         ,   rules:
             {
-                "personalDetails.firstName":
+                "currentPassword":
                 {
                     required:               true
-                }
-            ,   "personalDetails.lastName":
-                {
-                    required:               true
-                }
-            ,   "username":
-                {
-                    required:               true
-                ,   email:                  true
                 ,   remoteBidxApi:
                     {
-                        url:                "validateUsername.fetch"
-
+                        url:                "memberValidatePassword.send"
+                    ,   paramKey:           "password"
                     }
-
                 }
-            ,   "location":
+            ,   "newPassword":
                 {
-                    bidxLocationRequired:               true
-                ,
+                    required:               true
+                ,   minlength:              5
+                }
+            ,   "newPasswordRepeat":
+                {
+                    required:               true
+                ,   equalTo:               "[name=newPassword]"
+                ,   minlength:              5
                 }
 
             }
@@ -51,31 +50,94 @@
             {
                 // Anything that is app specific, the general validations should have been set
                 // in common.js already
+
             }
         ,   submitHandler:  function()
             {
-                if ( $btnRegister.hasClass( "disabled" ) )
+                if ( $btnUpdateCredentials.hasClass( "disabled" ) )
                 {
                     bidx.utils.log("button disabled");
                     return;
                 }
 
-                $btnRegister.addClass( "disabled" );
+                $btnUpdateCredentials.addClass( "disabled" );
 
-                _doRegister(
+                _doPasswordChange(
                 {
                     error: function( jqXhr )
                     {
-                        $btnRegister.removeClass( "disabled" );
+                        $btnUpdateCredentials.removeClass( "disabled" );
                     }
                 } );
             }
         } );
 
 
-    };
+    }
+
+    // generic view function. Hides all views and then shows the requested view. In case State argument is passed in, it will be used to show the title tag of that view
+    //
+    function _showView( view, state )
+    {
+        var $view = $views.hide().filter( bidx.utils.getViewName( view ) ).show();
+        //  show title of the view if available
+        if( state )
+        {
+            $view.find( ".title").hide().filter( bidx.utils.getViewName( state, "title" ) ).show();
+        }
+    }
+
+        // display generic error view with msg provided
+    //
+    function _showError( msg )
+    {
+        $views.filter( ".viewError" ).find( ".errorMsg" ).text( msg );
+        _showView( "error" );
+    }
 
 
+
+    function _doPasswordChange()
+    {
+
+        // Build up the data for the member request
+        //
+        var postData =
+        {
+            username:                   bidx.common.getSessionValue( "username" )
+        ,   password:                   $frmCredentials.find( "[name='currentPassword']" ).val()
+        ,   newPassword:                $frmCredentials.find( "[name='newPassword']" ).val()
+        };
+
+        bidx.api.call(
+            "memberChangepassword.update"
+        ,   {
+                groupDomain:    bidx.common.groupDomain
+            ,   data:           postData
+            ,   success:        function( response )
+                {
+                    bidx.utils.log( "member.save::success::response", response );
+
+                    $btnUpdateCredentials.removeClass( "disabled" );
+                    _showView( "credentialsChanged" );
+
+
+
+                }
+            ,   error:          function( jqXhr )
+                {
+                    var response = $.parseJSON( jqXhr.responseText )
+                        status = bidx.utils.getValue( jqXhr, "status" ) || textStatus;
+                    ;
+
+                    $btnUpdateCredentials.removeClass( "disabled" );
+
+                    _showError( "Something went wrong changing the password: (" + status + ") " + response.code );
+
+                }
+            }
+        );
+    }
 
 
     // generic view function. Hides all views and then shows the requested view
@@ -98,7 +160,7 @@
 
 
 
-     //   _showView( "register" );
+        _showView( "credentials" );
 
 
     };
