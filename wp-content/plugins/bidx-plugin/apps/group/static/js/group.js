55b188fd0ed362d7301212be7c91d53b6501b12e
arne <arne@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-11-04 09:18:54 +0000

    BIDX-1198 : maximize the unread counter to 99

--- wp-content/plugins/bidx-plugin/apps/group/static/js/group.js
+++ wp-content/plugins/bidx-plugin/apps/group/static/js/group.js
@@ -7,13 +7,23 @@
     ,   $unreadCount    = $navbar.find( ".iconbar-unread" )
     ;
 
+    // Whenever we get a new mailbox state, update the value of the unread count to reflect this
+    // Maximize to 99, above that abbreviate it to 99+
+    //
     $bidx.on( "mailboxState", function( e, mailboxState )
     {
-        var unread = bidx.utils.getValue( mailboxState, "Inbox.unread" );
+        var unread = parseInt( bidx.utils.getValue( mailboxState, "Inbox.unread" ), 10 );
 
-        if ( typeof unread === "undefined" )
+        if ( isNaN( unread ) )
         {
             unread = "?";
+        }
+        else
+        {
+            if ( unread > 99 )
+            {
+                unread = "99+";
+            }
         }
 
         $unreadCount.text( unread );
