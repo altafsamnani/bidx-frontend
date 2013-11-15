37bf4ee5bbcaa241b7ae83b8092d2e461bcbaddd
sakis.katsikas <sakis.katsikas@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-11-15 13:36:29 +0000

    Layout fixes & post template fix

--- wp-content/themes/bidx-group-template/templates/content-single.php
+++ wp-content/themes/bidx-group-template/templates/content-single.php
@@ -1,6 +1,6 @@
 
 <?php while (have_posts()) : the_post(); ?>
-<br>
+
 <div class="container">
   <div class="row-fluid">
 
@@ -8,7 +8,7 @@
 
     // Add the proper class for the layout depending on if there is a category linked to the post
     $spansize = '';
-    $categories = get_the_category(); 
+    $categories = get_the_category();
     foreach ($categories as $category) {
       $countcat = count($categories);
       if ($countcat == 1 && $category->slug == 'uncategorized') {
@@ -38,43 +38,47 @@
           <?php the_tags('<ul class="entry-tags"><li>','</li><li>','</li></ul>'); ?>
 
         </footer>
-        <?php 
-        //  comments_template('/templates/comments.php'); 
+        <?php
+        //  comments_template('/templates/comments.php');
         ?>
       </article>
-      </div> 
+      </div>
 
 <?php endwhile; ?>
 
 <?php
     // Wordpress shifts the 'uncategorized' category to be the last object in this array
     if ( $categories[0]->slug != 'uncategorized' ) :
-?> 
-    <div class="span4 well">
-      <ul class="nav nav-list">
-      <li class="nav-header"><h3><?php echo $categories[0]->name; ?></h3></li>
-<?php
-      $id = get_the_ID();
-      // The Query
-      query_posts( array ( 'category_name' => $categories[0]->slug, 'posts_per_page' => -1 ) );
-      // The Loop
-      while ( have_posts() ) : the_post();
-      if ($categories[0]->object_id == $id) { ?>
-        <li class="active"><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
-<?php 
-      } else {
 ?>
-        <li><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
+    <div class="span4 ">
+      <br>
+      <div class="well">
+        <ul class="nav nav-list">
+        <li class="nav-header"><h3><?php echo $categories[0]->name; ?></h3></li>
 <?php
-      }
-      endwhile;
-      // Reset Query
-      wp_reset_query();
+        $id = get_the_ID();
+        // The Query
+        query_posts( array ( 'category_name' => $categories[0]->slug, 'posts_per_page' => -1 ) );
+        // The Loop
+        while ( have_posts() ) : the_post();
+        if ($categories[0]->object_id == $id) {
 ?>
-      </ul>
+          <li class="active"><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
+<?php
+        } else {
+?>
+          <li><a href="<?php the_permalink() ?>" title="<?php the_title(); ?>"><?php the_title(); ?></a></li>
+<?php
+        }
+        endwhile;
+        // Reset Query
+        wp_reset_query();
+?>
+        </ul>
+      </div>
     </div>
-<?php 
+<?php
     endif;
 ?>
-    </div> 
-  </div> 
+    </div>
+  </div>
