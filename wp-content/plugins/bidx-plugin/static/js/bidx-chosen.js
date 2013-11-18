488d6a0e0d5f5971443258d58f4d082ac0600bc6
arne <arne@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-11-18 17:18:40 +0000

    added bidx-chosen jQuery plugin, which is a wrapper over the bidx static data api and the chosen plugin

---
+++ wp-content/plugins/bidx-plugin/static/js/bidx-chosen.js
@@ -0,0 +1,110 @@
+/* global bidx */
+;(function( $ )
+{
+    var pluginName  = "bidx_chosen"
+    ,   defaults    =
+        {
+            chose:
+            {
+                "search_contains":              true
+            ,   "width":                        "100%"
+            }
+        }
+    ;
+
+    // Plugin constructor which keeps the state of the instantiated plugin
+    //
+    function Plugin( element, options )
+    {
+        this.$element   = $( element );
+
+        this.settings   = $.extend( {}, defaults, options );
+        this._defaults  = defaults;
+        this._name      = pluginName;
+
+        this.init();
+    }
+
+    Plugin.prototype =
+    {
+        init: function ()
+        {
+            var plugin = this;
+
+            if ( plugin.settings.emptyValue )
+            {
+                plugin.$element.append(
+                    $(
+                        "<option />"
+                    ,   {
+                            value: ""
+                        }
+                    ).text( plugin.settings.emptyValue )
+                );
+            }
+
+            // Retrieve the items from the static data API
+            //
+            bidx.data.getContext( this.settings.dataKey, function( err, items )
+            {
+                plugin.items = items;
+
+                // Convert the items retrieved from the static API into <option />s
+                //
+                plugin.populateItems();
+
+                // Instantiate the chosen plugin
+                //
+                plugin.$element.chosen( plugin.settings.chosen );
+
+                // Inform the world we are ready
+                //
+                plugin.$element.trigger( pluginName + ":ready" );
+            } );
+
+            // Make sure the validator plugin picks up the selected value for validation
+            //
+            plugin.$element.on( "change", function()
+            {
+                plugin.$element.trigger( "click" );
+            } );
+        }
+
+        // Convert the list of retrieved items into options
+        //
+    ,   populateItems: function()
+        {
+            var plugin = this;
+
+            if ( !plugin.items || $.type( plugin.items ) !== "array" )
+            {
+                return;
+            }
+
+            $.each( plugin.items, function( i, option )
+            {
+                var $option = $(
+                    "<option />"
+                ,   {
+                        value:      option.value
+                    }
+                ).text( option.label );
+
+                plugin.$element.append( $option );
+            } );
+        }
+    };
+
+    // Make sure we only get instantiated once
+    //
+    $.fn[ pluginName ] = function ( options )
+    {
+        return this.each(function()
+        {
+            if ( !$.data( this, "plugin_" + pluginName ) )
+            {
+                $.data( this, "plugin_" + pluginName, new Plugin( this, options ) );
+            }
+        });
+    };
+} ( jQuery ));
