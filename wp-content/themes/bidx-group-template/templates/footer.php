cbb3e9138a2058e5bffa58c7233edd0bed238f30
sakis.katsikas <sakis.katsikas@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-12-12 15:57:50 +0000

    Changing icons

--- wp-content/themes/bidx-group-template/templates/footer.php
+++ wp-content/themes/bidx-group-template/templates/footer.php
@@ -18,17 +18,17 @@
 <footer>
     <div class="page-footer bidx-theme-colour-footer">
         <div class="container">
-            <div class="footer-bar">
-            	<div class="pull-left left-block">
-            		<div class="follow-us">Follow us</div>
-            	 	<div class="inline-list social-links">
-                        <a target="_blank" href="https://twitter.com/bid_x" class="sprite twitter"></a>
-                        <a target="_blank" href="https://www.facebook.com" class="sprite facebook"></a>
-                        <a target="_blank" href="http://www.linkedin.com/company/bidx" class="sprite linkedin"></a>
-                    </div>
+            <div class="footer-bar row">
+            	<div class="pull-left left-block col-sm-6">
+            		<div class="follow-us pull-left">Follow us</div>
+            	 	<ul class="nav nav-pills">
+                        <li><a target="_blank" href="https://twitter.com/bid_x"><i class="fa fa-twitter-square"></i></a></li>
+                        <li><a target="_blank" href="https://www.facebook.com"><i class="fa fa-facebook-square"></i></a></li>
+                        <li><a target="_blank" href="http://www.linkedin.com/company/bidx"><i class="fa fa-linkedin-square"></i></a></li>
+                    </ul>
             	</div>
 
-            	<div class="pull-right right-block">
+            	<div class="pull-right right-block col-sm-6 text-right">
             		<div class="copyright">&copy; <?php echo date("Y") ?>. bidX.net. <?php _e('All rights reserved','bidxtheme')?></div>
                     <div class="inline-list footer-menu">
             			<div><a href="/sitemap"><?php _e('Sitemap','bidxtheme')?></a></div>
