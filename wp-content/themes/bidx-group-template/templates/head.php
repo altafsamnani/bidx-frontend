5c5367aeb4ee49a770bbcf715296bdeadfba5f78
sakis.katsikas <sakis.katsikas@d71b490c-bffd-4f7d-827c-445c93e326aa>
2013-12-27 11:46:52 +0000

    Added one more browser condition

--- wp-content/themes/bidx-group-template/templates/head.php
+++ wp-content/themes/bidx-group-template/templates/head.php
@@ -2,7 +2,8 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" <?php language_attributes(); ?>> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" <?php language_attributes(); ?>> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"  <?php language_attributes(); ?>> <!--<![endif]-->
+<!--[if IE 9]>         <html class="no-js lt-ie10 lt-ie9" <?php language_attributes(); ?>> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js modern-browser"  <?php language_attributes(); ?>> <!--<![endif]-->
 <head>
 	<meta charset="utf-8">
 	<title><?php bloginfo('name'); ?> : <?php wp_title(); ?></title>
