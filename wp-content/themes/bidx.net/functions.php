e519f0adeaf2c6732b4daa9514e827c86c887f70
jaap.gorjup <jaap.gorjup@d71b490c-bffd-4f7d-827c-445c93e326aa>
2014-02-19 22:59:45 +0000

    Custom post types for management of content blocks.

--- wp-content/themes/bidx.net/functions.php
+++ wp-content/themes/bidx.net/functions.php
@@ -145,7 +145,37 @@
 			return $classes;
 	}
 	add_filter('post_class', 'has_thumb_class');
-
 	
+	/**
+	 * Add custom post types
+	 */
+
+	add_action ('init', 'bidx_create_post_types');
+	
+	function bidx_create_post_types() {
+		create_post_type();
+		create_post_type('group');
+		create_post_type('competition');
+		create_post_type('testimonial');
+	}
+	
+	function create_post_type($post_type = 'staff') {
+
+		$args = array (
+				'public' => true,
+				'exclude_from_search' => false,
+				'show_ui' => true,
+				'show_in_menu' => true,
+				'query_var' => true,
+				'rewrite' => false,
+				'capability_type' => 'post',
+				'_builtin' => false,
+				'has_archive' => false,
+				'hierarchical' => false,
+				'supports' => array( 'title', 'editor', 'thumbnail', 'excerpt' )
+		);
+		register_post_type ($post_type, $args);
+	
+	}
 
 ?>
