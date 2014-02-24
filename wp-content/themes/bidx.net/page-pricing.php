4884403f727b9d623b6269974cd91194f7a0ac85
sakis.katsikas <sakis.katsikas@d71b490c-bffd-4f7d-827c-445c93e326aa>
2014-02-24 13:45:23 +0000

    bidx.net theme:: Added pricing template

---
+++ wp-content/themes/bidx.net/page-pricing.php
@@ -0,0 +1,134 @@
+<?php get_header(); ?>
+
+            <div id="pricing-tables" class="main-wrapper">
+                <section id="pricing-tables" class="pad-25 pattern-1">
+                    <div class="container">
+                        <div class="pad-0">
+                            <div class="subpage-title">
+                                <h5>Pricing & Plans</h5>
+                            </div>
+                            <div class="row flush">
+                                <div class="col-md-3">
+                                    <div class="pricing groups">
+                                        <div class="p-header">
+                                            <div class="title">
+                                                <h4>GROUPS</h4>
+                                            </div>
+                                            <div class="price">
+                                                <span class="dollar">$</span>
+                                                <h2>6800</h2><br>
+                                                <span class="per">/ year</span>
+                                            </div>
+                                        </div>
+                                        <ul class="details">
+                                            <li><strong>Max Users</strong><br> Unlimited</li>
+                                            <li><strong>Discounts</strong><br> >10 Groups 30% less</li>
+                                            <li><strong>Customization</strong><br> $1000 per day</li>
+                                            <li><strong>Delivery time</strong><br> Max 4 weeks</li>
+                                            <li><strong>Customer support response time</strong><br> 2 working days, max</li>
+                                        </ul>
+                                        <div class="p-footer">
+                                            <a href="#" class="btn btn-flat flat-primary">Purchase Now</a>
+                                        </div>
+                                    </div>
+                                    <!-- /.pricing -->
+                                </div>
+                                <!-- /.col-md-4 -->
+                                <div class="col-md-3">
+                                    <div class="pricing competition">
+                                        <div class="p-header">
+                                            <div class="title">
+                                                <h4>COMPETITION</h4>
+                                            </div>
+                                            <div class="price">
+                                                <span class="dollar">$</span>
+                                                <h2>6800</h2><br>
+                                                <span class="per">1st time</span>
+                                            </div>
+                                        </div>
+                                        <ul class="details">
+                                            <li><strong>Max Users</strong><br> Unlimited</li>
+                                            <li><strong>Discounts</strong><br> $2000 after the 1st time</li>
+                                            <li><strong>Customization</strong><br> $1000 per day</li>
+                                            <li><strong>Delivery time</strong><br> Max 2 weeks</li>
+                                            <li><strong>Customer support response time</strong><br> 2 working days, max</li>
+                                        </ul>
+                                        <div class="p-footer">
+                                            <a href="#" class="btn btn-flat flat-primary">Purchase Now</a>
+                                        </div>
+                                    </div>
+                                    <!-- /.pricing -->
+                                </div>
+                                <!-- /.col-md-4 -->
+                                <div class="col-md-3">
+                                    <div class="pricing hotline">
+                                        <div class="p-header">
+                                            <div class="title">
+                                                <h4>HOTLINE</h4>
+                                            </div>
+                                            <div class="price">
+                                                <span class="dollar">$</span>
+                                                <h2>$6800</h2><br>
+                                                <span class="per">/year</span>
+                                            </div>
+                                        </div>
+                                        <ul class="details">
+                                            <li><strong>Max Users</strong><br> Unlimited</li>
+                                            <li><strong>Discounts</strong><br> No discounts applicable</li>
+                                            <li><strong>Customization</strong><br> $1000 per day</li>
+                                            <li><strong>Delivery time</strong><br> Max 4 weeks</li>
+                                            <li><strong>Customer support response time</strong><br> 2 working days, max</li>
+                                        </ul>
+                                        <div class="p-footer">
+                                            <a href="#" class="btn btn-flat flat-primary">Purchase Now</a>
+                                        </div>
+                                    </div>
+                                    <!-- /.pricing -->
+                                </div>
+                                <div class="col-md-3">
+                                    <div class="pricing ecosystem">
+                                        <div class="p-header">
+                                            <div class="title">
+                                                <h4>ECOSYSTEM</h4>
+                                            </div>
+                                            <div class="price">
+                                                <span class="dollar">$</span>
+                                                <h2>500000</h2><br>
+                                                <span class="per">/1st 2 years</span>
+                                            </div>
+                                        </div>
+                                        <ul class="details">
+                                            <li><strong>Max Users</strong><br> Unlimited</li>
+                                            <li><strong>Discounts</strong><br> Negotiable for small populations</li>
+                                            <li><strong>Customization</strong><br> All in</li>
+                                            <li><strong>Delivery time</strong><br> 2 year project</li>
+                                            <li><strong>Customer support response time</strong><br> 2 working days, max</li>
+                                        </ul>
+                                        <div class="p-footer">
+                                            <a href="#" class="btn btn-flat flat-primary">Purchase Now</a>
+                                        </div>
+                                    </div>
+                                    <!-- /.pricing -->
+                                </div>
+                                <!-- /.col-md-4 -->
+                            </div>
+                            <!-- /.row -->
+                        </div>
+                        <!-- /.pad-0 -->
+                    </div>
+                    <!-- /.container -->
+                </section>
+                <section>
+                    <div class="container service pad-25">
+                        <div class="col-sm-12">
+                            <h2>Want to create a Group with bidX?<br>Contact us to get started</h2>
+                             <button type="button" class="btn btn-flat flat-warning btn-lg btn-bordered">Contact Us</button>
+                        </div>
+                    </div>
+                </section>
+                <!-- /#pricing-tables -->
+            </div>
+
+
+
+<?php get_footer(); ?>
