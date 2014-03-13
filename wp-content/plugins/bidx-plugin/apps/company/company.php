d1055ea9275a8099bb550eda7d23753f5652e207
sakis.katsikas <sakis.katsikas@d71b490c-bffd-4f7d-827c-445c93e326aa>
2014-03-13 14:24:29 +0000

    BIDX-1807: Added dependency from bidx-reflowrower

--- wp-content/plugins/bidx-plugin/apps/company/company.php
+++ wp-content/plugins/bidx-plugin/apps/company/company.php
@@ -11,7 +11,7 @@ class company
     public $scriptInject;
 
     static $deps = array ('jquery', 'bootstrap', 'underscore', 'backbone', 'json2','gmaps-places', 'holder', 'bidx-utils', 'bidx-api-core', 'bidx-common', 'bidx-data', 'bidx-i18n',
-                              'jquery-validation', 'jquery-validation-additional-methods', 'jquery-validation-bidx-additional-methods', 'bidx-chosen');
+                              'jquery-validation', 'jquery-validation-additional-methods', 'jquery-validation-bidx-additional-methods', 'bidx-chosen', 'bidx-reflowrower');
 
     /**
      * Constructor
