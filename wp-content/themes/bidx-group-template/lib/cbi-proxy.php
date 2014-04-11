38e155143c57c487f61f2093e76a880872639f52
jaap.gorjup <jaap.gorjup@d71b490c-bffd-4f7d-827c-445c93e326aa>
2014-04-11 13:15:02 +0000

    Updated proxy to cache the data in a location that is theme independent.

--- wp-content/themes/bidx-group-template/lib/cbi-proxy.php
+++ wp-content/themes/bidx-group-template/lib/cbi-proxy.php
@@ -3,46 +3,63 @@
 require_once("../../../../wp-load.php");
 
 $base = "http://www.cbi.eu";
+$cache_base = "/wp-content/uploads/cbi/cache";
+
 if ( isset($_REQUEST['URL']) ) {
-	$url =  $base . str_replace ( ' ', '%20', $_REQUEST['URL']);
+	$url =  $base . str_replace ( ' ', '%20', $_REQUEST['URL'] );
 	
-	//TODO : check if in cache /wp-content/uploads/cbi
-	
-	$args = array(
-			'timeout'     => 10,
-			'redirection' => 5,
-			'httpversion' => '1.0',
-			'user-agent'  => 'bidx/' . $wp_version . '; ' . get_bloginfo( 'url' ),
-			'blocking'    => true,
-			'headers'     => array(),
-			'cookies'     => array(),
-			'body'        => null,
-			'compress'    => false,
-			'decompress'  => false,
-			'sslverify'   => false,
-			'stream'      => false,
-			'filename'    => null
-	);
-	
-	$response = wp_remote_get( $url, $args );
-	
-	if ( !is_wp_error( $response ) && 200 == $response['response']['code'] ) {
+	$lookup = str_replace( '/','__', $_REQUEST['URL']);
 		
-		$type = wp_remote_retrieve_header( $get, 'content-type' );
-		header('Content-type: ' . $type);
-		echo $response["data"];
-		$body = wp_remote_retrieve_body( $response );
-		
-		//TODO : store $body in cache
-		
-		echo $body;
+	//check the file location : if exists load it
+	$upload_dir = trailingslashit( WP_CONTENT_DIR ) . 'uploads/cbi/cache/';
+	$filename = $upload_dir . $lookup;
 	
+	if ( file_exists( $filename ) ) {
+		
+		$lastpart = strrpos( $filename , '__' );
+		if (strpos( $filename, '.js', $lastpart ) > 0 ) {
+			$type = 'application/javascript';
+		} else {
+			$finfo = finfo_open(FILEINFO_MIME_TYPE);
+			$type = finfo_file( $finfo, $filename );
+		}
+		header ( 'Content-type: ' . $type ) ;
+		include( $filename );
+
 	} else {
-		print_r($url);
-		print_r($response);
-		error_log("Cannot load ". $url . " using CBI Proxy");
+	
+		//else try to stream the file to a location
+		$args = array(
+				'timeout'     => 10,
+				'redirection' => 5,
+				'httpversion' => '1.1',
+				'user-agent'  => 'bidx/' . $wp_version . '; ' . get_bloginfo( 'url' ),
+				'blocking'    => true,
+				'headers'     => array(),
+				'cookies'     => array(),
+				'body'        => null,
+				'compress'    => false, 
+				'decompress'  => false,
+				'sslverify'   => false,
+				'stream'      => true,
+				'filename'    => $filename
+		);
 		
+		$response = wp_remote_get( $url, $args );
+		
+		if ( !is_wp_error( $response ) && 200 == $response['response']['code'] ) {
+			
+			$type = wp_remote_retrieve_header( $get, 'content-type' );
+			header('Content-type: ' . $type);
+			$body = wp_remote_retrieve_body( $response );
+			echo $body;
+		
+		} else {
+			print_r($response);
+			error_log("Cannot load ". $url . " using CBI Proxy");	
+		}
 	}
+	
 } else {
 	
 	error_log("No URL defined using CBI Proxy");
--- wp-content/themes/bidx-group-template/templates/content-cbi.php
+++ wp-content/themes/bidx-group-template/templates/content-cbi.php
@@ -6,49 +6,62 @@
 
 <?php
 	$base = "http://www.cbi.eu";
+	$cachetime = 300;
 	if ( isset($_REQUEST['URL']) ) {
 		$url = $base . $_REQUEST['URL'];
 	} else {
 		$url = $base . "/marketintel_platform/platform/177482/affiliate";
 	}
 	
-	/**
-	 * Optionally add storage here for performance enhancement
-	 */
-	$args = array(
-		'timeout'     => 5,
-		'redirection' => 5,
-		'httpversion' => '1.0',
-		'user-agent'  => 'bidx/' . $wp_version . '; ' . get_bloginfo( 'url' ),
-		'blocking'    => true,
-		'headers'     => array(),
-		'cookies'     => array(),
-		'body'        => null,
-		'compress'    => false,
-		'decompress'  => true,
-		'sslverify'   => false,
-		'stream'      => false,
-		'filename'    => null
-	);
-	$response = wp_remote_get( $url, $args );
-	$data = $response['body'];
-	
-	$self_base = get_permalink( $post->ID ); 
-	
-	//rewrite images that are relative
-	$data = str_replace("src=\"/", "src=\"/wp-content/themes/bidx-group-template/lib/cbi-proxy.php?URL=/", $data);
-	//rewrite images that are explicit
-	$data = str_replace("src=\"".$base, "src=\"/wp-content/themes/bidx-group-template/lib/cbi-proxy.php?URL=", $data);	
-	//rewrite javascript linkup
-	$data = str_replace("http://' + window.location.host", $self_base ."?URL='", $data);
-	//rewrite direct links
-	$data = str_replace("href=\"/","href=\"" . $self_base ."?URL=/", $data);
+	if( false === ($data = get_transient($transient_name) ) ) {
 
-
+		$args = array(
+				'timeout'     => 10,
+				'redirection' => 5,
+				'httpversion' => '1.1',
+				'user-agent'  => 'bidx/' . $wp_version . '; ' . get_bloginfo( 'url' ),
+				'blocking'    => true,
+				'headers'     => array(),
+				'cookies'     => array(),
+				'body'        => null,
+				'compress'    => false,
+				'decompress'  => true,
+				'sslverify'   => false,
+				'stream'      => false,
+				'filename'    => null
+		);
+		$response = wp_remote_get( $url, $args );
+		
+		if (is_wp_error($response)) {
+		
+			$data = "<div id=\"well error\">Could not load the data referenced <br/><div style=\"color: red\">". $response->get_error_message() ."</div>";
+		
+		} else {
+		
+			$data = wp_remote_retrieve_body( $response );
+		
+			$self_base = get_permalink( $post->ID );
+		
+			//rewrite images that are relative (regex can be used for replacing full string and encode the url)
+			$data = str_replace( "src=\"/", "src=\"/wp-content/themes/bidx-group-template/lib/cbi-proxy.php?URL=/", $data );
+			//rewrite images that are explicit
+			$data = str_replace( "src=\"".$base, "src=\"/wp-content/themes/bidx-group-template/lib/cbi-proxy.php?URL=", $data );
+			//rewrite javascript linkup
+			$data = str_replace( "http://' + window.location.host", $self_base ."?URL='" , $data );
+			//rewrite direct links
+			$data = str_replace( "href=\"/","href=\"" . $self_base ."?URL=/", $data );
+			//remove script elements
+			//$data = preg_replace( "/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/i", "", $data );
+			//$data = preg_replace( "/<style\b[^<]*(?:(?!<\/style>)<[^<]*)*<\/style>/i", "", $data );
+			
+			set_transient( $url, $data, $cachetime);
+		}		
+	} 
+	
 	?>
 	<div class="container cbi-content">
 		<div id="cbi-container">
-			<?php print_r( $data ); ?>
+			<?php echo $data ?>
 		</div>
 	</div>
 
@@ -60,3 +73,4 @@
     		});
 		});
 	</script>
+	
