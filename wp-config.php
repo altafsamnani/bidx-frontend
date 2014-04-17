8c3e94fad6e0620b5ecb1d0123331be018d416db
jaap.gorjup <jaap.gorjup@d71b490c-bffd-4f7d-827c-445c93e326aa>
2014-04-17 11:19:10 +0000

    Cleaned up version of wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -14,43 +14,51 @@
  * @package WordPress
  */
 
-// ** MySQL settings - You can get this info from your web host ** //
+/**
+ * 	--- Environment specific settings ---
+ */
+
+/** what environment : local/test/acceptance or empty for production**/
+define('BIDX_ENV', 'local');
+
+//replace this by automatic value and connect to backend.test.bidnet.vc:8080
+define('API_DOMAIN', 'test.bidx.net');
+//autofill this by environment
+define('WP_DEVELOPMENT', true);
+
+//these values should NOT be in the generic defines (hackable)
+//ideally we remove the need for the username/password and use another mechanism
+/** The username for admin access to the API **/
+define('API_AUTH_UNAME','bidx');
+/** The password for admin access to the API **/
+define('API_AUTH_PASS','gobidx');
+
 /** The name of the database for WordPress */
 define('DB_NAME', 'bidx');
-
 /** MySQL database username */
 define('DB_USER', 'bidx');
-
 /** MySQL database password */
 define('DB_PASSWORD', 'bidx12345');
-
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
 
+/**
+ * 	--- End of environment specific settings ---
+ */
+
 /** Database Charset to use in creating database tables. */
 define('DB_CHARSET', 'utf8');
-
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
+define('DOMAIN_CURRENT_SITE', BIDX_ENV . '.bidx.net');
 
-/**#@+
+/**
  * Authentication Unique Keys and Salts.
- *
  * Change these to different unique phrases!
  * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
  * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
- *
  * @since 2.6.0
  */
-define('AUTH_KEY',         'put your unique phrase here');
-define('SECURE_AUTH_KEY',  'put your unique phrase here');
-define('LOGGED_IN_KEY',    'put your unique phrase here');
-define('NONCE_KEY',        'put your unique phrase here');
-define('AUTH_SALT',        'put your unique phrase here');
-define('SECURE_AUTH_SALT', 'put your unique phrase here');
-define('LOGGED_IN_SALT',   'put your unique phrase here');
-define('NONCE_SALT',       'put your unique phrase here');
-/**
 define('AUTH_KEY',         '%(h1mMtZ~t>7pC};H/,>J|[O|/I^Q TflEIa+%PUj|B~M6H8%&c+T.}F(|XI_hGb');
 define('SECURE_AUTH_KEY',  '$]z{Vq[z,$T3GT0S&dP5&niTC)6r=W ^HG s2!rWJ-(5(NqCk]F8?bOb#+{vnnch');
 define('LOGGED_IN_KEY',    'nd)1MJy|?]:c6j:UP9h4.M7Rz>OE),9Wkv1mc?I_B+Qf-{Hw.0{R6vt+!w^c<ap@');
@@ -59,26 +67,24 @@ define('AUTH_SALT',        '?.5|_2);@?mEt((B4I,<MG6#xa2Q-|I9#W53y3n*,nJ({BjR*>B`
 define('SECURE_AUTH_SALT', '}{krku(L8-#]TB/3IEc%X=x?y@OeBt?O1z$)k`qD~yd.&fD/8%l{1kk|?&7y9N2+');
 define('LOGGED_IN_SALT',   '76B.h(:0)|RxpqUc}X?iDl|8p;Q|Wf6H_-tVei7e>gz&&;V,|?^8v|y_{`#^6wz-');
 define('NONCE_SALT',       '-hDJc^&:M&c=/@)6.;rw1T%dFT;HnWO9h+u*6mY0xspAX6L6KcMP<~|f8B%A0-7w');
-*/
 
 /**
+ * Cookie settings
  * Ensuring cookies are shared
+ * Create unique cookies for environments
  */
 define('ADMIN_COOKIE_PATH', '/');
-define('COOKIE_DOMAIN', 'local.bidx.net');
+define('COOKIE_DOMAIN', 'bidx.net');
 define('COOKIEPATH', '/');
 define('SITECOOKIEPATH', '/');
-
+define('LOGGED_IN_COOKIE', 'bidx_logged_in' . BIDX_ENV . '_' . COOKIEHASH);
 
 define('SCRIPT_DEBUG', false);
 define('CONCATENATE_SCRIPTS', true);
 define('COMPRESS_SCRIPTS', true);
 
-/**#@-*/
-
 /**
  * WordPress Database Table prefix.
- *
  * You can have multiple installations in one database if you give each a unique
  * prefix. Only numbers, letters, and underscores please!
  */
@@ -96,42 +102,38 @@ define('WPLANG', '');
 
 /**
  * For developers: WordPress debugging mode.
- *
  * Change this to true to enable the display of notices during development.
  * It is strongly recommended that plugin and theme developers use WP_DEBUG
  * in their development environments.
  */
-define('WP_DEBUG_DISPLAY', false);
-
-define('WP_DEBUG', true);
-
-define('WP_DEBUG_LOG', true);
-
-define('SAVEQUERIES', true);
-
+if (WP_DEVELOPMENT) {
+	define('WP_DEBUG_DISPLAY', false);
+	define('WP_DEBUG', true);
+	define('WP_DEBUG_LOG', true);
+	define('SAVEQUERIES', true);
+} else {
+	define('WP_DEBUG_DISPLAY', false);
+	define('WP_DEBUG', false);
+	define('WP_DEBUG_LOG', false);
+	define('SAVEQUERIES', false);
+}
 define('FS_METHOD','direct');
-
-define('API_AUTH_UNAME','bidx');
-
-define('API_AUTH_PASS','gobidx');
 
 /**
  * Internal settings for the API
  **/
-define('API_URL','http://test.bidx.net/api/v1/');
-define('IMG_URL','https://test.bidx.net/dl/document/');
+define('API_URL','http://' . API_DOMAIN . '/api/v1/');
+define('IMG_URL','https://' . DOMAIN_CURRENT_SITE . '/dl/document/');
 
 /* Multisite */
 define('WP_ALLOW_MULTISITE', true);
 define('MULTISITE', true);
 define('SUBDOMAIN_INSTALL', true);
-define('DOMAIN_CURRENT_SITE', 'local.bidx.net');
 define('PATH_CURRENT_SITE', '/');
 define('SITE_ID_CURRENT_SITE', 1);
 define('BLOG_ID_CURRENT_SITE', 1);
+define('NOBLOGREDIRECT', 'http://' . DOMAIN_CURRENT_SITE . '/unknown-group');
 
-define('WP_DEVELOPMENT', true);
-define('NOBLOGREDIRECT', 'http://' . DOMAIN_CURRENT_SITE);
 define('BIDX_PLUGIN_METADATA_PATH', 'http://bidx-builds.s3-website-eu-west-1.amazonaws.com/acceptance/bidx-plugin/metadata.json');
 
 /* That's all, stop editing! Happy blogging. */
@@ -142,3 +144,4 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
