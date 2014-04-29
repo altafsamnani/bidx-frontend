aa1f522728c02d29653ffd397070e7a37ebaaa03
jaap.gorjup <jaap.gorjup@d71b490c-bffd-4f7d-827c-445c93e326aa>
2014-04-29 15:26:22 +0000

    BIDX-1810 : arranged selector in customizer to add multiple google analytics codes next to that of bidx

--- wp-content/themes/bidx-group-template/templates/head.php
+++ wp-content/themes/bidx-group-template/templates/head.php
@@ -26,19 +26,27 @@
 			echo "</style>";
 		}
  ?>
-	<script type="text/javascript">
+ 
+ <!-- Google Analytics -->
+<script>
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', 'UA-619772-9', 'auto');
+<?php 
+	$analytics = explode( ',' , get_theme_mod( 'analytics_codes' ) );
+	foreach ( $analytics as $key ) {
+		//add check if not empty
+		if ( !empty ( trim( $key ) ) ) {
+?>ga('create', '<?php echo trim( $key ) ?>' , 'auto');
+<?php 
+		}
+	}
+?>
+ga('send', 'pageview');
+</script>
 
-	  var _gaq = _gaq || [];
-	  _gaq.push(['_setAccount', 'UA-619772-9']);
-	  _gaq.push(['_trackPageview']);
-
-	  (function() {
-	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-	  })();
-
-	</script>
 
 </head>
 
