557ae2d1adf13a13ec2df9972af1c37d5b892871
altaf.samnani <altaf.samnani@d71b490c-bffd-4f7d-827c-445c93e326aa>
2014-04-29 14:45:17 +0000

    Upgrading of theme bidx.net

--- wp-content/themes/bidx.net/functions.php
+++ wp-content/themes/bidx.net/functions.php
@@ -1,5 +1,15 @@
 <?php
+//Initialize the update checker.
+require ABSPATH . 'wp-content/plugins/bidx-plugin/theme-updates/theme-update-checker.php';
+
+$theme_update_checker = new ThemeUpdateChecker(
+	'bidx.net',                                            //Theme folder name, AKA "slug". 
+	BIDX_THEME_METADATA_PATH //URL of the metadata file.
+);
+
+
 load_theme_textdomain ('bidxtheme', WP_CONTENT_DIR . '/languages/themes');
+
 require_once locate_template('/lib/utils.php');           // Utility functions
 require_once locate_template ('/lib/init.php');            // Initial theme setup and constants
 require_once locate_template ('/lib/nav.php');             // Custom nav modifications
