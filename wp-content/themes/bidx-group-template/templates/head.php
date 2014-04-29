a79ac2852181a3fd666c3d67a174ff0df3007677
jaap.gorjup <jaap.gorjup@d71b490c-bffd-4f7d-827c-445c93e326aa>
2014-04-29 15:33:45 +0000

    updated the GA code for bidx.net management

--- wp-content/themes/bidx-group-template/templates/head.php
+++ wp-content/themes/bidx-group-template/templates/head.php
@@ -33,7 +33,7 @@
 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
 m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-619772-9', 'auto');
+ga('create', 'UA-48404092-2', 'auto');
 <?php 
 	$analytics = explode( ',' , get_theme_mod( 'analytics_codes' ) );
 	foreach ( $analytics as $key ) {
