092ede34e18fb3fcf8772ef44c0286621c096073
altaf.samnani <altaf.samnani@d71b490c-bffd-4f7d-827c-445c93e326aa>
2014-05-22 16:42:11 +0000

    Again

--- wp-content/themes/bidx-group-template/templates/searchform.php
+++ wp-content/themes/bidx-group-template/templates/searchform.php
@@ -1,8 +1,8 @@
-<div class="form-inline" id="search">
+<form action="" class="form-inline" id="search">
     <div class="input-group">
         <input type="text" class="form-control form-q" name="qMain" placeholder="Search">
         <span class="input-group-btn">
-            <a class="btn btn-default btn-search" data-href="/search#search/list"><i class="fa fa-search"></i></a>
+            <button class="btn btn-default btn-search" data-href="/search#search/list" type="submit"><i class="fa fa-search"></i></button>
         </span>
     </div>
-</div>
+</form>
