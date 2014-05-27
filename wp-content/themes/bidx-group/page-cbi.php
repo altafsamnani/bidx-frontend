c0ce2d2d5b403dd45cb01eaefd1d23864d06f593
jaap.gorjup <jaap.gorjup@d71b490c-bffd-4f7d-827c-445c93e326aa>
2014-05-27 13:40:26 +0000

    removed CBI references

--- wp-content/themes/bidx-group/page-cbi.php
+++
@@ -1,8 +0,0 @@
-<?php 
-/*
-Template Name: CBI Market Intelligence template
-*/
-?>
-
-<?php // get_template_part('templates/page', 'header'); ?>
-<?php get_template_part('templates/content', 'cbi'); ?>
