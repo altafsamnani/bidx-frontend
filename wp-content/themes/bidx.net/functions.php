8edd04cf16236ddb8638b0d0afd474b2be316bce
jaap.gorjup <jaap.gorjup@d71b490c-bffd-4f7d-827c-445c93e326aa>
2014-06-06 09:36:46 +0000

    Disabled the Theme update checker because it leads to unwanted messages

--- wp-content/themes/bidx.net/functions.php
+++ wp-content/themes/bidx.net/functions.php
@@ -1,11 +1,13 @@
 <?php
 //Initialize the update checker.
-require ABSPATH . 'wp-content/plugins/bidx-plugin/theme-updates/theme-update-checker.php';
+//Disabled by Jaap
+//
+// require ABSPATH . 'wp-content/plugins/bidx-plugin/theme-updates/theme-update-checker.php';
 
-$theme_update_checker = new ThemeUpdateChecker(
-	'bidx.net',                                            //Theme folder name, AKA "slug". 
-	BIDX_THEME_METADATA_PATH //URL of the metadata file.
-);
+// $theme_update_checker = new ThemeUpdateChecker(
+// 	'bidx.net',                                            //Theme folder name, AKA "slug". 
+// 	BIDX_THEME_METADATA_PATH //URL of the metadata file.
+// );
 
 
 load_theme_textdomain ('bidxtheme', WP_CONTENT_DIR . '/languages/themes');
