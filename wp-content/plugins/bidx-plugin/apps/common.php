4b1aaa3721416b88c84d90d0fbf5d107bb9eb5d7
jaap.gorjup <jaap.gorjup@d71b490c-bffd-4f7d-827c-445c93e326aa>
2014-06-17 13:44:45 +0000

    Added check for new class in array since it gives warnings in the log.

--- wp-content/plugins/bidx-plugin/apps/common.php
+++ wp-content/plugins/bidx-plugin/apps/common.php
@@ -716,11 +716,16 @@ class BidxCommon
             $body = $templateLibrary->replaceMessageTokens ($message->content);
             $subject = $templateLibrary->replaceMessageTokens ($message->subject);
 
+            if ( !isset($transientI18nData['templates'][$countMessage]) ) {
+            	$transientI18nData['templates'][$countMessage] = new stdClass();
+            }
             $transientI18nData['templates'][$countMessage] = new stdClass();
             $transientI18nData['templates'][$countMessage]->value = $message->name.'subject';
             $transientI18nData['templates'][$countMessage]->label = $subject;
             $countMessage++;
-
+            if ( !isset($transientI18nData['templates'][$countMessage]) ) {
+            	$transientI18nData['templates'][$countMessage] = new stdClass();
+            }
             $transientI18nData['templates'][$countMessage] = new stdClass();
             $transientI18nData['templates'][$countMessage]->value = $message->name.'body';
             $transientI18nData['templates'][$countMessage]->label = $body;
