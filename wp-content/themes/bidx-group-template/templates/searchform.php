e984fc4ad70fda4856ef09e7297a1a6345468f5d
altaf.samnani <altaf.samnani@d71b490c-bffd-4f7d-827c-445c93e326aa>
2014-07-07 14:33:20 +0000

    Translation bugs resolved

--- wp-content/themes/bidx-group-template/templates/searchform.php
+++ wp-content/themes/bidx-group-template/templates/searchform.php
@@ -1,6 +1,6 @@
 <form action="" class="form-inline" id="search">
     <div class="input-group">
-        <input type="text" class="form-control form-q" name="qMain" placeholder="Search">
+        <input type="text" class="form-control form-q" name="qMain" placeholder="<?php _e('Search','bidxplugin');?>">
         <span class="input-group-btn">
             <button class="btn btn-default btn-search" data-href="/search#search/list" type="submit"><i class="fa fa-search"></i></button>
         </span>
