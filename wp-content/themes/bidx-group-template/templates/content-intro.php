e723a9804b29fcb5f238406ba7035ff635f23a78
Sakis Katsikas <sakis@bidx.net>
2014-09-12 17:15:53 +0200

    BIDX-2469

--- wp-content/themes/bidx-group-template/templates/content-intro.php
+++ wp-content/themes/bidx-group-template/templates/content-intro.php
@@ -2,13 +2,15 @@
 
     // for now a shortcode call in the template. will be replaced to page.xml asap
     // To preview in customize in themes we are directly renderint that file
-    if(is_super_admin()) {
+    if ( is_super_admin() )
+    {
         require_once( BIDX_PLUGIN_DIR . '/templatelibrary.php' );
         $view = new TemplateLibrary( BIDX_PLUGIN_DIR . '/group/templates/' );
-        $view->render( 'group-home-public.phtml' );
-    } else {
-        
-    echo do_shortcode( '[bidx app="group" view="home"]' );
+        $view->render( 'group-home-private.phtml' );
+    }
+    else
+    {
+        echo do_shortcode( '[bidx app="group" view="home"]' );
     }
     
 ?>
