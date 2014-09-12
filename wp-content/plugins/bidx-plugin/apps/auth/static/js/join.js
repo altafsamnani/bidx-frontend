891f876ce8a33a13cbe8ea49f030a7a4b248855a
Sakis Katsikas <sakis@bidx.net>
2014-09-12 18:13:28 +0200

    BIDX-2400

--- wp-content/plugins/bidx-plugin/apps/auth/static/js/join.js
+++ wp-content/plugins/bidx-plugin/apps/auth/static/js/join.js
@@ -973,6 +973,14 @@
 
     window.bidx.join = app;
 
+    // Hot Fix for BIDX-2400
+    // http://stackoverflow.com/questions/7131909/facebook-callback-appends-to-return-url
+    //
+    if ( window.location.hash && window.location.hash === '#_=_')
+    {
+        window.location.hash = '';
+    }
+
     if ($("body.bidx-join").length && !bidx.utils.getValue(window, "location.hash").length)
     {
         if ( authenticated && hasProfile && joinedCurrentGroup )
