ebb291a0833e595b63bb31c7ab60259c059f1e87
Sakis Katsikas <sakis@bidx.net>
2014-09-23 16:00:32 +0200

    Added a different link for an active competition for anonymous user to direct them to /join

--- wp-content/themes/bidx-group/templates/content-single-competition.php
+++ wp-content/themes/bidx-group/templates/content-single-competition.php
@@ -7,14 +7,18 @@
         $endTimestamp   = strtotime($endDate);
         $is_running     = ( $endTimestamp > $nowTimestamp )     ? TRUE : FALSE;
         $coming_soon    = ( $startTimestamp > $nowTimestamp )   ? TRUE : FALSE;
-        // d($startDate);
-        // d($startTimestamp);
-        // d($nowTimestamp);
-        // d($post);
-?>
-<?php
-            if ( $coming_soon )
-            {
+
+        $session = BidxCommon::$staticSession;
+
+        $authenticated = false;
+
+        if ( isset( $session->authenticated ) && $session->authenticated == 'true' )
+        {
+            $authenticated = true;
+        }
+
+        if ( $coming_soon )
+        {
 ?>
             <blockquote class="alert-info hide-overflow">
                 <span class="pull-left"><?php  _e('Starts at:','roots'); ?>
@@ -22,19 +26,33 @@
                 <a class="btn pull-right btn-info" href="<?php echo _l('competition'); ?>"><?php  _e('Go back to overview','roots'); ?></a>
             </blockquote>
 <?php               
-            }
-            elseif ( $is_running )
-            {
+        }
+        elseif ( $is_running )
+        {
 ?>
             <blockquote class="alert-success hide-overflow">
                 <span class="pull-left"><?php  _e('Apply before:','roots'); ?>
                 <strong><?php echo $endDate; ?></strong></span>
+<?php 
+        if ( $authenticated )
+        {
+?>
                 <a class="btn pull-right btn-success" href="<?php echo _l('competition'); ?>"><?php  _e('Apply now','roots'); ?></a>
+<?php                
+        }
+        else
+        {
+?>
+                <a class="btn pull-right btn-success" href="<?php echo _l('join'); ?>"><?php  _e('Register to apply','roots'); ?></a>
+<?php                
+        }
+?>
+
             </blockquote>
 <?php                
-            }
-            else
-            {
+        }
+        else
+        {
 ?>
             <blockquote class="alert-danger hide-overflow">
                 <span class="pull-left"><?php  _e('Expired at:','roots'); ?>
@@ -42,17 +60,17 @@
                 <a class="btn pull-right btn-danger" href="<?php echo _l('competition'); ?>"><?php  _e('Go back to overview','roots'); ?></a>
             </blockquote>
 <?php                
-            }
+        }
 ?>            
 <?php 
-            if ( has_post_thumbnail() ) { // check if the post has a Post Thumbnail assigned to it.
-              the_post_thumbnail('large', array('class' => 'img-responsive'));
-            } 
+        if ( has_post_thumbnail() )
+        { // check if the post has a Post Thumbnail assigned to it.
+            the_post_thumbnail('large', array('class' => 'img-responsive'));
+        } 
 ?>
-            <h1><?php the_title(); ?></h1>
-            <div><?php the_excerpt(); ?></div>
-            <div>
-                <?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?>
-            </div>
-          <div><?php the_content(); ?></div>
+        <h1><?php the_title(); ?></h1>
+        <div><?php the_excerpt(); ?></div>
+        <div><?php wp_link_pages(array('before' => '<nav class="page-nav"><p>' . __('Pages:', 'roots'), 'after' => '</p></nav>')); ?></div>
+        <div><?php the_content(); ?></div>
+        
 <?php endwhile; ?>
