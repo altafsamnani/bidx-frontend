68c7a9970468de98d9bec2d6f14318cd538a7999
Sakis Katsikas <sakis@bidx.net>
2015-01-26 14:42:55 +0100

    Industry codes sorting

--- wp-content/plugins/bidx-plugin/static/js/bidx-industries.js
+++ wp-content/plugins/bidx-plugin/static/js/bidx-industries.js
@@ -44,7 +44,19 @@
             }
         } );
 
+        sectorSort(mainSectorArr);
+        sectorSort(subSectorArr);
+        sectorSort(endSectorArr);
     } );
+
+    function sectorSort( ar )
+    {
+        ar.sort(function(a,b){
+            if(a.value > b.value){ return 1;}
+            if(a.value < b.value){ return -1;}
+            return 0;
+        });
+    }
 
     $.widget( "bidx.industries",
     {
