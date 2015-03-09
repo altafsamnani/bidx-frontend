9b6af9a1d4a7af38263b6ea20f09aee80fe0749a
Sakis Katsikas <sakis@bidx.net>
2015-03-09 18:26:33 +0100

    BIDX-2950

--- wp-content/themes/bidx-group/templates/entry-meta.php
+++ wp-content/themes/bidx-group/templates/entry-meta.php
@@ -1,1 +1,12 @@
+
+<?php $post_categories = wp_get_post_categories( $post->ID ); ?>
+
 <time class="published time" datetime="<?php echo get_the_time('c'); ?>"><?php echo get_the_date(); ?></time>
+
+<?php if ( $post_categories ): ?>
+    <div class="time"><span><?php _e('Categories','bidxplugin'); ?></span>: <span class="wp-categories"><?php the_category(', '); ?></span></div>
+<?php endif; ?>
+
+<div class="time"><span><?php the_tags( 'Tags: ', ', ', '<br />' ); ?></span></div>
+<hr>
+
