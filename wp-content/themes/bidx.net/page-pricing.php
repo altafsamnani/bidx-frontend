94c1c29f35a55806bac80eaa684b3c65cfb019ef
Altaf <altaf@bidx.net>
2015-09-29 14:59:20 +0200

    Pricing table by Sakis

--- wp-content/themes/bidx.net/page-pricing.php
+++ wp-content/themes/bidx.net/page-pricing.php
@@ -1,36 +1,179 @@
 <?php get_header(); ?>
-<section id="page-title-wrapper" class="page-title-wrapper pattern-1 service">
-    <div class="container">
-        <h1><?php the_title(); ?></h1>
-    </div>
-</section>
-<div id="pricing-tables" class="main-wrapper">
-    <section id="pricing-tables" class="pad-25 pattern-7">
+    <section id="page-title-wrapper" class="page-title-wrapper pattern-1 service">
         <div class="container">
-            <?php the_content(); ?>
+            <h1><?php the_title(); ?></h1>
         </div>
     </section>
-    <section class="pattern-3"> 
-        <div class="container service pad-25">
-            <div class="col-sm-12">
-                <h2>
-                    <?php echo _e('Find Out More About Add-Ons', 'bidxtheme'); ?>
-                </h2>
-                <a href="/support" type="button" class="btn btn-flat flat-primary btn-lg"><?php echo _e('Support', 'bidxtheme'); ?></a>
-                <a href="/training" type="button" class="btn btn-flat flat-primary btn-lg"><?php echo _e('Training', 'bidxtheme'); ?></a>
+    <div id="pricing-tables" class="main-wrapper">
+        <section id="pricing-tables" class="pad-25 pattern-7">
+            <div class="container">
+                <?php the_content(); ?>
+
+
+<h2>Portal &amp; Competition</h2>
+<h4>(Annual prices)</h4>
+<table class="table">
+<thead>
+<tr>
+<th></th>
+<th>Do it Yourself</th>
+<th>Basic</th>
+<th>Pro</th>
+<th>Advanced</th>
+<th>Competition</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>Portal</td>
+<td>√</td>
+<td>√</td>
+<td>√</td>
+<td>√</td>
+<td>√</td>
+</tr>
+<tr>
+<td>Set up of portal</td>
+<td></td>
+<td>√</td>
+<td>√</td>
+<td>√</td>
+<td>√</td>
+</tr>
+<tr>
+<td>Start up training</td>
+<td></td>
+<td>√</td>
+<td>√</td>
+<td>√</td>
+<td>√</td>
+</tr>
+<tr>
+<td>Service desk</td>
+<td></td>
+<td>√</td>
+<td></td>
+<td></td>
+<td></td>
+</tr>
+<tr>
+<td>Service desk Pro</td>
+<td></td>
+<td></td>
+<td>√</td>
+<td>√</td>
+<td>√</td>
+</tr>
+<tr>
+<td>Marketing</td>
+<td></td>
+<td></td>
+<td></td>
+<td>√</td>
+<td>√</td>
+</tr>
+<tr>
+<td>Monitoring</td>
+<td></td>
+<td></td>
+<td></td>
+<td>√</td>
+<td>√</td>
+</tr>
+<tr>
+<td>Competition</td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td>√</td>
+</tr>
+<tr>
+<td>Total ($ USD)</td>
+<td>$1,999</td>
+<td>$6,950</td>
+<td>$8,950</td>
+<td>$10,450</td>
+<td>$12,450</td>
+</tr>
+</tbody>
+</table>
+<h2>Hotline</h2>
+<table class="table">
+<thead>
+<tr>
+<th></th>
+<th>6 Months</th>
+<th>Year</th>
+<th>Advanced</th>
+</tr>
+</thead>
+<tbody>
+<tr>
+<td>Hotline</td>
+<td>√ (½)</td>
+<td>√</td>
+<td>√</td>
+</tr>
+<tr>
+<td>Service desk</td>
+<td>√ (½)</td>
+<td>√</td>
+<td></td>
+</tr>
+<tr>
+<td>Service desk</td>
+<td>√ (½)</td>
+<td>√</td>
+<td></td>
+</tr>
+<tr>
+<td>Service desk Pro</td>
+<td></td>
+<td></td>
+<td>√</td>
+</tr>
+<tr>
+<td>Review desk</td>
+<td></td>
+<td></td>
+<td>√</td>
+</tr>
+<tr>
+<td>Total</td>
+<td>$1,499</td>
+<td>$2,950</td>
+<td>$5,950</td>
+</tr>
+</tbody>
+</table>
+<h3>We also offer monthly and hourly rates for Competitions and Service desk, please do not hesitate to contact us for a tailored proposal.</h3>
+
+
+
             </div>
-        </div>
-    </section>
-    <section>
-        <div class="container service pad-25">
-            <div class="col-sm-12">
-                <h2>
-                    <?php echo _e('Contact us to get started', 'bidxtheme'); ?>
-                </h2>
-                <a href="/get-started" type="button" class="btn btn-flat flat-warning btn-lg"><?php echo _e('Get started', 'bidxtheme'); ?></a>
+        </section>
+        <section class="pattern-3">
+            <div class="container service pad-25">
+                <div class="col-sm-12">
+                    <h2>
+                        <?php echo _e('Find Out More About Add-Ons', 'bidxtheme'); ?>
+                    </h2>
+                    <a href="/support" type="button" class="btn btn-flat flat-primary btn-lg"><?php echo _e('Support', 'bidxtheme'); ?></a>
+                    <a href="/training" type="button" class="btn btn-flat flat-primary btn-lg"><?php echo _e('Training', 'bidxtheme'); ?></a>
+                </div>
             </div>
-        </div>
-    </section>
-    <!-- /#pricing-tables -->
-</div>
+        </section>
+        <section>
+            <div class="container service pad-25">
+                <div class="col-sm-12">
+                    <h2>
+                        <?php echo _e('Contact us to get started', 'bidxtheme'); ?>
+                    </h2>
+                    <a href="/get-started" type="button" class="btn btn-flat flat-warning btn-lg"><?php echo _e('Get started', 'bidxtheme'); ?></a>
+                </div>
+            </div>
+        </section>
+        <!-- /#pricing-tables -->
+    </div>
 <?php get_footer(); ?>
