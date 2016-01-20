bdeec19474cf339b43df8b06e45bcb3743d44586
Julia Pop <julia@bidx.net>
2016-01-20 11:34:41 +0100

    Accent issue

--- wp-content/plugins/bidx-plugin/static/js/i18n.js
+++ wp-content/plugins/bidx-plugin/static/js/i18n.js
@@ -8,7 +8,7 @@
     ,   iclVars             = window.icl_vars || {}
     ,   currentLanguage     = ( iclVars.current_language ) ? iclVars.current_language : 'en'
     ,   rtlLangArr          = ['ar']
-    ,   accentLang          = ['es', 'fr', 'mz']
+    ,   accentLang          = ['es', 'mz']
     ;
 
         // Internal administration of cached items, let's keep it for now in an object, maybe later sync it to localstorage
