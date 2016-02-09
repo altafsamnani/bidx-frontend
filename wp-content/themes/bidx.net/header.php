33f1bb0c598a26a752b2510eb660520ee46da907
Tarek Amr <gr33ndata@yahoo.com>
2016-02-09 16:11:39 +0100

    BIDX-4061 Adding Facebook OG Meta tags and Tracking Pixel

--- wp-content/themes/bidx.net/header.php
+++ wp-content/themes/bidx.net/header.php
@@ -62,6 +62,32 @@
 
 	<!-- <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'template_url' ); ?>/theme.css" /> -->
 	<!-- <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" /> -->
+
+    <!-- Facebook OG Meta Tags -->
+    <meta property="og:locale" content="en_US">
+    <meta property="og:image" content='https://bidx.net/wp-content/themes/bidx.net/assets/img/bidxgroupicon.png'/>
+    <meta property="og:url" content="http://bidx.net/"/>
+    <meta property="og:title" content="bidx"/>
+    <meta property="og:site_name" content="bidx | Entrepreneurial ecosystems online">
+    <meta property="og:description" content="Start, grow and finance your entrepreneurs. Connect them with investors, mentors and business partners.">
+    <!-- End of Facebook OG Meta Tags -->
+
+    <!-- Facebook Pixel Code -->
+    <script>
+      !function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+      n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;
+      n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;
+      t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,
+      document,'script','//connect.facebook.net/en_US/fbevents.js');
+
+      fbq('init', '543324655849061');
+      fbq('track', "PageView");
+    </script>
+    <noscript>
+      <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=543324655849061&ev=PageView&noscript=1"/>
+    </noscript>
+    <!-- End Facebook Pixel Code -->
+
 </head>
 
 <body <?php body_class(); ?>>
